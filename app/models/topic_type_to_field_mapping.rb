34d2f5e8fb08d714beb35527a5d3a54b3a9cc212
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-16 02:31:16 +0000

    cleaned up a depreciated way of declaring a belongs_to in a couple places
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@19 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_to_field_mapping.rb
+++ app/models/topic_type_to_field_mapping.rb
@@ -1,7 +1,7 @@
 class TopicTypeToFieldMapping < ActiveRecord::Base
   belongs_to :topic_type
   belongs_to :topic_type_field
-  belongs_to :form_field, :class_name => "TopicTypeField"
-  belongs_to :required_form_field, :class_name => "TopicTypeField"
+  belongs_to :form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
+  belongs_to :required_form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   acts_as_list :scope => :topic_type_id
 end
