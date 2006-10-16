8d9386a793428b3cda219bbf875589551a3a4ed1
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-16 03:26:21 +0000

    added some comments for tricky logic of hash of hashes
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@20 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/controllers/topic_types_controller.rb
+++ app/controllers/topic_types_controller.rb
@@ -3,12 +3,23 @@ class TopicTypesController < ApplicationController
   def add_to_topic_type
     topic_type = TopicType.find(params[:id])
 
+    # this is setup for a form that has multiple fields
     # we want to separate out plain form fields from required ones
+
+    # params has a hash of hashes for field with field_id as the key
     params[:topic_type_field].keys.each do |field_id|
       field = TopicTypeField.find_by_id(field_id)
+
+      # into the field's hash
+      # now we can grab the field's attributes that are being updated
       params[:topic_type_field][field_id].keys.each do |to_add_attr|
         to_add_attr_value = params[:topic_type_field][field_id][to_add_attr]
+
+        # if we are supposed to add the field
         if to_add_attr_value.to_i == 1
+
+          # determine if it should be a required field
+          # or just an optional one
           if to_add_attr =~ /required/
             topic_type.required_form_fields << field
           else
