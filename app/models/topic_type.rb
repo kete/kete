a25449f1099b9e4bf1d326ee2b6e7caece263531
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-16 00:48:56 +0000

    after some experimentation, this is the first real pass at these models
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@14 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type.rb
+++ app/models/topic_type.rb
@@ -1,2 +1,25 @@
 class TopicType < ActiveRecord::Base
+  has_many :topic_type_to_field_mappings, :dependent => :destroy, :order => 'position'
+  # Walter McGinnis (walter@katipo.co.nz), 2006-10-05
+  # these association extension maybe able to be cleaned up with modules or something in rails proper down the line
+  # code based on work by hasmanythrough.com
+  # you have to do the elimination of dupes through the sql
+  # otherwise, rails will reorder by topic_type_to_field_mapping.id after the sql has bee run
+  has_many :form_fields, :through => :topic_type_to_field_mappings, :source => :topic_type_field, :select => "distinct topic_type_to_field_mappings.position, topic_type_fields.*", :order => 'position' do
+    def <<(topic_type_field)
+      TopicTypeToFieldMapping.with_scope(:create => { :required => "false"}) { self.concat topic_type_field }
+    end
+  end
+  has_many :required_form_fields, :through => :topic_type_to_field_mappings, :source => :required_form_field, :select => "distinct topic_type_to_field_mappings.position, topic_type_fields.*", :conditions => "topic_type_to_field_mappings.required = 'true'", :order => 'position', :uniq => true do
+    def <<(required_form_field)
+      TopicTypeToFieldMapping.with_scope(:create => { :required => "true"}) { self.concat required_form_field }
+    end
+  end
+  validates_presence_of :name, :description
+  validates_uniqueness_of :name
+  # globalize stuff, uncomment later
+  # translates :name, description
+  def available_fields
+    @available_fields = TopicTypeField.find_available_fields(self)
+  end
 end
---
+++ app/models/topic_type_field.rb
@@ -0,0 +1,21 @@
+class TopicTypeField < ActiveRecord::Base
+  has_many :topic_type_to_field_mappings, :dependent => :destroy
+  has_many :topic_type_forms, :through => :topic_type_to_field_mappings, :source => :topic_type, :order => 'position'
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  # globalize stuff, uncomment later
+  # translates :name, description
+  def self.find_available_fields(topic_type)
+    find(:all, :readonly => false,
+         :conditions => ["id not in (select topic_type_field_id from topic_type_to_field_mappings where topic_type_id = ?)", topic_type.id])
+  end
+  # TODO: make this DRY!
+  def add_checkbox
+    # used by a form of available fields where 0 is always going to be the starting value
+    return 0
+  end
+  def required_checkbox
+    # used by a form of available fields where 0 is always going to be the starting value
+    return 0
+  end
+end
