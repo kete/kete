6328c30191a51d70187e9bc1e32a119d35ff3c96
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-17 01:14:28 +0000

    first round of test for topic_type_to_field_mapping, note that a good amount of it's functionality is covered by topic_types test
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@28 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- test/unit/topic_type_to_field_mapping_test.rb
+++ test/unit/topic_type_to_field_mapping_test.rb
@@ -12,4 +12,43 @@ class TopicTypeToFieldMappingTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
+
+  def setup
+    @person_type = topic_types(:person)
+    @place_type = topic_types(:place)
+    @name_field = topic_type_fields(:name)
+    @capacity_field = topic_type_fields(:capacity)
+  end
+
+  ## not much in this model yet, but lets test that acts_as_list is working
+
+  # test it shouldn't be possible to have a null position
+  def test_null_position
+    topic_type_to_field_mapping = TopicTypeToFieldMapping.new(:topic_type_id => @place_type.id,
+                                                              :topic_type_field_id => @capacity_field.id)
+    assert topic_type_to_field_mapping.save
+    assert topic_type_to_field_mapping.position
+  end
+
+  def test_reorder_position
+    mapping1 = @person_type.topic_type_to_field_mappings[0]
+    mapping2 = @person_type.topic_type_to_field_mappings[1]
+
+    orig_mapping2_id = mapping2.id
+
+    assert mapping1.position.to_i < mapping2.position.to_i
+
+    mapping2.move_to_top
+
+    # i found this a bit confusing, you have to refresh the object
+    # after manipulating it's list
+    @person_type = TopicType.find_by_name(@person_type[:name])
+
+    mapping1 = @person_type.topic_type_to_field_mappings[0]
+    mapping2 = @person_type.topic_type_to_field_mappings[1]
+
+    assert mapping1.position.to_i < mapping2.position.to_i
+    assert_equal mapping1.id, orig_mapping2_id
+  end
+
 end
