0ec9d00a8ef868f285d5bbcb089ef899a3dc75bd
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-18 02:17:41 +0000

    added comment about tests
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@33 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_to_field_mapping.rb
+++ app/models/topic_type_to_field_mapping.rb
@@ -4,4 +4,6 @@ class TopicTypeToFieldMapping < ActiveRecord::Base
   belongs_to :form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   belongs_to :required_form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   acts_as_list :scope => :topic_type_id
+  # position should never be null, but it's not directly in a form at this point, so not validating here
+  # there is a test for it though
 end
