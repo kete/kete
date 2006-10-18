c541fe8e07e24e2a5950794eca5b0a6b2e6079cb
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-18 04:15:51 +0000

    adding versioning to topics as per acts_as_versioned
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@37 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ db/migrate/005_add_versions_to_topics.rb
@@ -0,0 +1,10 @@
+class AddVersionsToTopics < ActiveRecord::Migration
+  def self.up
+    # create versioning table for topics
+    Topic.create_versioned_table
+  end
+
+  def self.down
+    Topic.drop_versioned_table
+  end
+end
