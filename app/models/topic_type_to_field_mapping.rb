a5f0b076e0ebc1c3e696ba7fd17184a7200070d6
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-19 00:50:19 +0000

    cleaned up some code using the piggyback plugin
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@39 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_to_field_mapping.rb
+++ app/models/topic_type_to_field_mapping.rb
@@ -4,6 +4,9 @@ class TopicTypeToFieldMapping < ActiveRecord::Base
   belongs_to :form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   belongs_to :required_form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   acts_as_list :scope => :topic_type_id
-  # position should never be null, but it's not directly in a form at this point, so not validating here
+  piggy_back :topic_type_field_name,
+      :from => :topic_type_field, :attributes => [:name]
+  # position should never be null, but it's not specified directly at creation at this point, so not validating here
   # there is a test for it though
+
 end
