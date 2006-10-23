e97093ad66bb0596b1d54db490b0ead312ad6a45
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-10-23 19:04:37 +0000

    switched to ajaxscaffold generator to topics and topic_types, first round of adding topic.xml_attributes based on topic_type form fields
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@43 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_to_field_mapping.rb
+++ app/models/topic_type_to_field_mapping.rb
@@ -4,8 +4,8 @@ class TopicTypeToFieldMapping < ActiveRecord::Base
   belongs_to :form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   belongs_to :required_form_field, :class_name => "TopicTypeField", :foreign_key => "topic_type_field_id"
   acts_as_list :scope => :topic_type_id
-  piggy_back :topic_type_field_name,
-      :from => :topic_type_field, :attributes => [:name]
+  piggy_back :topic_type_field_name_and_description,
+      :from => :topic_type_field, :attributes => [:name, :description]
   # position should never be null, but it's not specified directly at creation at this point, so not validating here
   # there is a test for it though
 
