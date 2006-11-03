a064600ae334d36063187271af6985d9d3353f62
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-11-03 02:34:57 +0000

    cleaned up some renaming of things
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@73 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/011_create_content_item_relations.rb
+++ db/migrate/011_create_content_item_relations.rb
@@ -1,16 +1,16 @@
-class CreateContentRelations < ActiveRecord::Migration
+class CreateContentItemRelations < ActiveRecord::Migration
   def self.up
-    create_table :content_relations do |t|
+    create_table :content_item_relations do |t|
       t.column :position, :integer, :null => false
       t.column :topic_id, :integer, :null => false
-      t.column :related_content_id, :integer, :null => false, :references => nil
-      t.column :related_content_type, :string, :null => false
+      t.column :related_item_id, :integer, :null => false, :references => nil
+      t.column :related_item_type, :string, :null => false
       t.column :created_at, :datetime, :null => false
       t.column :updated_at, :datetime, :null => false
     end
   end
 
   def self.down
-    drop_table :content_relations
+    drop_table :content_item_relations
   end
 end
