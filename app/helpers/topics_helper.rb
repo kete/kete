711db8f192bdda2e6e8fdc9b613b395aa7b8869c
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-11-03 05:03:39 +0000

    added a rough helper for displaying xml_attributes, added show action, template, and modified _topic.rhtml to link to it
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@74 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -8,5 +8,14 @@ module TopicsHelper
   def scaffold_columns
     Topic.scaffold_columns
   end
+  # above ajaxscaffold generated helpers, now for ours
 
+  def display_xml_attributes(topic)
+    html_string = ""
+    topic.xml_attributes.each do |field_key, field_value|
+      field_name = field_key.humanize
+      html_string += "<p> #{field_name}: #{field_value} </p>\n"
+    end
+    return html_string
+  end
 end
