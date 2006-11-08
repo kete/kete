7c451abce9d9fa5cf94e4e8507e57d16b15f5340
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-11-08 22:54:27 +0000

    changed server to db in text
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@83 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ test/unit/zoom_server_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ZoomDbTest < Test::Unit::TestCase
+  fixtures :zoom_dbs
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
