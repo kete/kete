b1ced0fe2d138e2b562d0a84bb5594bf85619d03
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-11-24 23:06:37 +0000

    moving ZoomDb model to acts_as_zoom plugin
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@99 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/zoom_db.rb
+++
@@ -1,6 +0,0 @@
-class ZoomDb < ActiveRecord::Base
-  validates_presence_of :database_name, :host, :port
-  validates_uniqueness_of :database_name, :scope => [:host, :port], :message => "The combination of database name, host, and port must be unique."
-  validates_numericality_of :port
-
-end
--- app/models/zoom_server.rb
+++
@@ -1,2 +0,0 @@
-class ZoomDb < ActiveRecord::Base
-end
