452828a62403d7a72f9b825b68ea9f1abbaef936
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-07 01:27:08 +0000

    renamed topic_type_field.name to label, added xml_element_name
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@109 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_field.rb
+++ app/models/topic_type_field.rb
@@ -2,11 +2,11 @@ class TopicTypeField < ActiveRecord::Base
   has_many :topic_type_to_field_mappings, :dependent => :destroy
   # if we ever use this association, we'll want to add a test for it
   has_many :topic_type_forms, :through => :topic_type_to_field_mappings, :source => :topic_type, :order => 'position'
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_presence_of :label
+  validates_uniqueness_of :label
   # TODO: add validation that prevents the generic topic fields from being re-added
   # globalize stuff, uncomment later
-  # translates :name, description
+  # translates :label, :description
   def self.find_available_fields(topic_type)
     find(:all, :readonly => false,
          :conditions => ["id not in (select topic_type_field_id from topic_type_to_field_mappings where topic_type_id = ?)", topic_type.id])
