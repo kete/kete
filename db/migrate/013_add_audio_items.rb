85243b52c712c85d8ef358a9ee2e44e64ddc8a0c
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-22 04:15:35 +0000

    moving audio_items to parent of audio_files
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@168 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/013_add_audio_items.rb
+++ db/migrate/013_add_audio_items.rb
@@ -3,10 +3,6 @@ class AddAudioItems < ActiveRecord::Migration
     create_table :audio_items do |t|
       t.column :title, :string, :null => false
       t.column :description, :text
-      t.column :content_type, :string, :limit => 100
-      t.column :filename,     :string, :limit => 255
-      t.column :path,         :string, :limit => 255
-      t.column :size,        :integer
       t.column :created_at, :datetime, :null => false
       t.column :updated_at, :datetime, :null => false
     end
