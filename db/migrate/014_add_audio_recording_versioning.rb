60a6e83b0e9e37734cb76be4d678a93c83e2ee32
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-23 01:50:01 +0000

    fixed typo
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@177 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/014_add_audio_recording_versioning.rb
+++ db/migrate/014_add_audio_recording_versioning.rb
@@ -1,10 +1,10 @@
 class AddAudioRecordingVersioning < ActiveRecord::Migration
   def self.up
     # create versioning table for audio_recordings
-    Audiorecording.create_versioned_table
+    AudioRecording.create_versioned_table
   end
 
   def self.down
-    Audiorecording.drop_versioned_table
+    AudioRecording.drop_versioned_table
   end
 end
