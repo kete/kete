a929e1341b47f83a25ac79b4a122191f0f9116f4
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-23 01:59:13 +0000

    adding private directory to store private files for a kete (basket)
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@178 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/audio_recording.rb
+++ app/models/audio_recording.rb
@@ -1,2 +1,26 @@
 class AudioRecording < ActiveRecord::Base
+  # this is where we handled "related to"
+  has_many :content_item_relations, :as => :related_item, :dependent => :destroy
+  has_many :topics, :through => :content_item_relations
+
+  # a virtual attribute that holds the audio_item's entire content (sans binary file)
+  # as xml formated how we like it
+  # for use by acts_as_zoom virtual_field_name, :raw => true
+  # this virtual attribue will be populated/updated in our controller
+  # in create and update
+  # i.e. before save, which triggers our acts_as_zoom record being shot off to zebra
+  attr_accessor :oai_record
+  acts_as_zoom :fields => [:oai_record], :save_to_public_zoom => ['localhost', 'public'], :raw => true
+
+  acts_as_versioned
+  validates_presence_of :title, :path, :filename
+  # this may change
+  validates_uniqueness_of :title
+  # TODO: add validation that prevents markup in short_summary
+  # globalize stuff, uncomment later
+  # translates :title, :description
+
+  has_attachment :storage => :file_system, :file_system_path => 'private', :content_type => ['audio/mpeg', 'audio/x-mpegurl', 'audio/x-wav', 'application/ogg']
+  validates_as_attachment
+
 end
---
+++ test/unit/audio_recording_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AudioRecordingTest < Test::Unit::TestCase
+  fixtures :audio_recordings
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
