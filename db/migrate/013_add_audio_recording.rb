9a915097fc1d5c819e7842166cf2cf64b5fb0afb
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-23 01:37:45 +0000

    added initial skeleton for audio... again
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@175 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ db/migrate/013_add_audio_recording.rb
@@ -0,0 +1,18 @@
+class AddAudioRecording < ActiveRecord::Migration
+  def self.up
+    create_table :audio_recordings do |t|
+      t.column :title, :string, :null => false
+      t.column :description, :text
+      t.column :path, :string, :null => false
+      t.column :filename, :string, :null => false
+      t.column :content_type, :string, :null => false
+      t.column :size, :integer, :null => false
+      t.column :created_at, :datetime, :null => false
+      t.column :updated_at, :datetime, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :audio_recordings
+  end
+end
