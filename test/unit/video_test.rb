0594423ae3e7a7abb5ead82bf4584f5da0ca446e
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-24 03:42:02 +0000

    adding video, based on audio code
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@191 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ test/unit/video_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class VideoTest < Test::Unit::TestCase
+  fixtures :videos
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
