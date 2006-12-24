635024860dcc361bfe8bcaf7bc54d8db1ad4e717
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-24 03:41:48 +0000

    adding video, based on audio code
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@189 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,67 @@
+class VideoController < ApplicationController
+  def index
+    redirect_to :action => 'index', :controller => '/search', :current_class => 'Video', :all => true
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    redirect_to :action => 'index', :controller => '/search', :current_class => 'Video', :all => true
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(params[:video])
+    # TODO: because id isn't available until after a save, we have a HACK
+    # to add id into record during acts_as_zoom
+    @video.oai_record = render_to_string(:template => 'video/oai_record',
+                                                   :layout => false)
+    @successful = @video.save
+
+    if params[:relate_to_topic_id] and @successful
+      ContentItemRelation.new_relation_to_topic(params[:relate_to_topic_id], @video)
+      # TODO: translation
+      flash[:notice] = 'The video was successfully created.'
+      # TODO: make this a helper
+      redirect_to :action => 'show', :controller => '/topics', :id => params[:relate_to_topic_id]
+    elsif @successful
+      # TODO: translation
+      flash[:notice] = 'The video was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    # TODO: because id isn't available until after a save, we have a HACK
+    # to add id into record during acts_as_zoom
+    @video.oai_record = render_to_string(:template => 'video/oai_record',
+                                                   :layout => false)
+    if @video.update_attributes(params[:video])
+      flash[:notice] = 'Video was successfully updated.'
+      redirect_to :action => 'show', :id => @video
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Video.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+end
---
+++ app/models/video.rb
@@ -0,0 +1,46 @@
+class Video < ActiveRecord::Base
+# this is where we handled "related to"
+  has_many :content_item_relations, :as => :related_item, :dependent => :destroy
+  has_many :topics, :through => :content_item_relations
+
+  # a virtual attribute that holds the video's entire content (sans binary file)
+  # as xml formated how we like it
+  # for use by acts_as_zoom virtual_field_name, :raw => true
+  # this virtual attribue will be populated/updated in our controller
+  # in create and update
+  # i.e. before save, which triggers our acts_as_zoom record being shot off to zebra
+  attr_accessor :oai_record
+  acts_as_zoom :fields => [:oai_record], :save_to_public_zoom => ['localhost', 'public'], :raw => true
+
+  acts_as_versioned
+  validates_presence_of :title
+  # this may change
+  validates_uniqueness_of :title
+  # TODO: add validation that prevents markup in short_summary
+  # globalize stuff, uncomment later
+  # translates :title, :description
+
+  # handles file uploads
+  # we'll want to adjust the filename to include "...-1..." for each
+  # version where "-1" is dash-version number
+  # for images this will include thumbnails
+  # this will require overriding full_filename method locally
+  # TODO: add more content_types
+  # processor none means we don't have to load expensive image manipulation
+  # dependencies that we don't need
+  # :file_system_path => "#{BASE_PRIVATE_PATH}/#{self.table_name}",
+  # will rework with when we get to public/private split
+  has_attachment :storage => :file_system, :file_system_path => "public/video", :content_type => ['application/x-shockwave-flash', 'video/mpeg', 'video/quicktime', 'video/x-msvideo'], :processor => :none
+  validates_as_attachment
+
+  # overriding full_filename to handle our customizations
+  # TODO: is this thumbnail arg necessary for classes without thumbnails?
+  # def full_filename(thumbnail = nil)
+    # file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:file_system_path].to_s
+    # this is how this currently reads
+    # rails_root/private/videos/recording_id/filename
+    # TODO: we'll want to make it like this when we add kete (basket) scoping
+    # rails_root/private/kete_path_name/videos/recording_id/filename
+    # File.join(RAILS_ROOT, file_system_path, attachment_path_id, thumbnail_name_for(thumbnail))
+  # end
+end
