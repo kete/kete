32e8a290188bd6aebb9bfa1c322c525fe1873750
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-26 21:49:49 +0000

    refactored for image_file model with image model as only meta info
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@205 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- test/unit/image_files_test.rb
+++ test/unit/image_files_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ImageThumbTest < Test::Unit::TestCase
-  fixtures :image_thumbs
+  fixtures :image_files
 
   # Replace this with your real tests.
   def test_truth
