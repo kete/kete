87473482c99e4d36fb80a6a1a129c48424b9910e
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2006-12-26 21:42:26 +0000

    moving to image_file for our model
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@203 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/017_add_images.rb
+++ db/migrate/017_add_images.rb
@@ -3,11 +3,6 @@ class AddImages < ActiveRecord::Migration
     create_table :images do |t|
       t.column :title, :string, :null => false
       t.column :description, :text
-      t.column :filename, :string, :null => false
-      t.column :content_type, :string, :null => false
-      t.column :size, :integer, :null => false
-      t.column :width, :integer
-      t.column :height, :integer
       t.column :created_at, :datetime, :null => false
       t.column :updated_at, :datetime, :null => false
     end
