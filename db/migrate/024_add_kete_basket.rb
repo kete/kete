1b1da8e3343195052d387470f64e2b9b67d43884
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-01-08 18:38:47 +0000

    first pass at populating default basket
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@242 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ db/migrate/024_add_kete_basket.rb
@@ -0,0 +1,15 @@
+class AddDefault
+  def self.up
+    Basket.create(:name => 'Default', :urlified_name => 'default')
+  end
+
+  def self.down
+    ImageFile.delete_all
+    StillImage.delete_all
+    Video.delete_all
+    AudioRecording.delete_all
+    WebLink.delete_all
+    Topic.delete_all
+    Basket.delete_all
+  end
+end
