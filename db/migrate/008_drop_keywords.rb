405eb47c3e89f5ec6ea5093f70330e7bf2f748c7
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-01-10 02:11:04 +0000

    adding drop of keywords from topic_versions
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@247 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/008_drop_keywords.rb
+++ db/migrate/008_drop_keywords.rb
@@ -1,9 +1,11 @@
 class DropKeywords < ActiveRecord::Migration
   def self.up
     remove_column :topics, :keywords
+    remove_column :topic_versions, :keywords
   end
 
   def self.down
     add_column :topics, :keywords, :text
+    add_column :topic_versions, :keywords, :text
   end
 end
