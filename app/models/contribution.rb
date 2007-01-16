0034fed373881c17d8a615c5ada64e546111912c
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-01-16 08:05:40 +0000

    first draft of contribution model and migration
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@276 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,2 +1,19 @@
 class Contribution < ActiveRecord::Base
+  # this is where we track our polymorphic contributions
+  # between users
+  # and multiple types of items
+  # the user can have multiple contributions
+  # for versions
+  # or multiple roles
+  belongs_to :user
+  belongs_to :contributed_item, :polymorphic => true
+  # by using has_many :through associations we gain some bidirectional flexibility
+  # with our polymorphic join model
+  # basicaly specifically name the classes on the other side of the relationship here
+  # see http://blog.hasmanythrough.com/articles/2006/04/03/polymorphic-through
+  belongs_to :web_link, :class_name => "WebLink", :foreign_key => "contributed_item_id"
+  belongs_to :audio_recording, :class_name => "AudioRecording", :foreign_key => "contributed_item_id"
+  belongs_to :video, :class_name => "Video", :foreign_key => "contributed_item_id"
+  belongs_to :still_image, :class_name => "StillImage", :foreign_key => "contributed_item_id"
+  belongs_to :topic, :class_name => "Topic", :foreign_key => "contributed_item_id"
 end
--- db/migrate/025_create_contributions.rb
+++ db/migrate/025_create_contributions.rb
@@ -1,6 +1,16 @@
 class CreateContributions < ActiveRecord::Migration
   def self.up
+    # a user can have multiple contributions to the same thing
+    # they may contribute in different roles
+    # or different versions
     create_table :contributions do |t|
+      t.column :user_id, :integer, :null => false
+      t.column :contributed_item_id, :integer, :null => false, :references => nil
+      t.column :contributed_item_type, :string, :null => false
+      t.column :role, :string, :null => false
+      t.column :version, :integer, :null => false
+      t.column :created_at, :datetime, :null => false
+      t.column :updated_at, :datetime, :null => false
     end
   end
 
