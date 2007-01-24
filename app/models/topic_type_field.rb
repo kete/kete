b6eba79ac3734242d946dfd35c44aba1ba9ce29f
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-01-24 02:05:06 +0000

    fixed syntax bug
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@314 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/topic_type_field.rb
+++ app/models/topic_type_field.rb
@@ -6,21 +6,24 @@ class TopicTypeField < ActiveRecord::Base
   validates_uniqueness_of :label
   # don't allow spaces
   validates_format_of :xml_element_name , :with => /^[^\s]*$/, :message => ": spaces aren't allowed"
+
   # TODO: add validation that prevents the generic topic fields from being re-added
-  # globalize stuff, uncomment later
+
+  # TODO: globalize stuff, uncomment later
   # translates :label, :description
+
   def self.find_available_fields(topic_type)
+    # exclude ancestor's fields as well
+    topic_types_to_exclude = topic_type.ancestors + [topic_type]
     find(:all, :readonly => false,
-         :conditions => ["id not in (select topic_type_field_id from topic_type_to_field_mappings where topic_type_id = ?)", topic_type.id])
+         :conditions => ["id not in (select topic_type_field_id from topic_type_to_field_mappings where topic_type_id in (?))", topic_types_to_exclude])
   end
-  # TODO: make this DRY!
-  # use alias
+
   def add_checkbox
     # used by a form of available fields where 0 is always going to be the starting value
     return 0
   end
-  def required_checkbox
-    # used by a form of available fields where 0 is always going to be the starting value
-    return 0
-  end
+
+  alias required_checkbox add_checkbox
+
 end
