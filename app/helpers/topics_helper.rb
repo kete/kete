419662d8acf684ae518ee103ec869a3bebd22065
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-01-24 23:19:20 +0000

    added indenting and helper for it for topic_type drop down
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@317 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,4 +1,23 @@
 module TopicsHelper
+  def topic_type_select_with_indent(object, method, collection, value_method, text_method, current_value, html_options={ })
+    result = "<select name='#{ object}[#{method}]'"
+    html_options.each do |key, value|
+        result << ' ' + key.to_s + '="' + value.to_s + '"'
+    end
+    result << ">\n"
+    for element in collection
+      indent_string = String.new
+        element.level.times { indent_string += "&nbsp;" }
+        if current_value == element.send(value_method)
+          result << "<option value='#{ element.send(value_method)}' selected='selected'>#{indent_string}#{element.send(text_method)}</option>\n"
+        else
+          result << "<option value='#{element.send(value_method)}'>#{indent_string}#{element.send(text_method)}</option>\n"
+        end
+    end
+    result << "</select>\n"
+    return result
+  end
+
   def display_xml_attributes(topic)
     html_string = ""
     # TODO: these should have their order match the specified order for the topic_type
