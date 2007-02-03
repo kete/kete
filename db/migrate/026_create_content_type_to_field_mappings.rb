e79638c462cf7279883d841271cb99386a4f39c5
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-02-03 23:11:29 +0000

    adding first draft migration of content type field mapping
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@353 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ db/migrate/026_create_content_type_to_field_mappings.rb
@@ -0,0 +1,16 @@
+class CreateContentTypeToFieldMappings < ActiveRecord::Migration
+  def self.up
+    create_table :content_type_to_field_mappings do |t|
+      t.column :content_type, :string, :null => false
+      t.column :extended_field_id, :integer, :null => false
+      t.column :position, :integer, :null => false
+      t.column :required, :boolean, :default => false
+      t.column :created_at, :datetime, :null => false
+      t.column :updated_at, :datetime, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :content_type_to_field_mappings
+  end
+end
