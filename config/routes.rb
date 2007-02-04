840475eb335ad12ad1ab247f356d5f05fb75a86c
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-02-04 19:00:49 +0000

    adding notes about new routes
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@354 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- config/routes.rb
+++ config/routes.rb
@@ -35,3 +35,47 @@ ActionController::Routing::Routes.draw do |map|
   # :all is true by default if there are no search_terms
   map.connect '', :controller => "search"
 end
+
+# route scratch
+
+# example site/all/topics/
+# basket/all/controller_name_for_zoom_class/ ->
+# all items of that zoom_class in basket, with current tab reflecting
+# if the user opts to search from this point, they go to basket/search/controller_name_for_zoom_class/for/search_term
+
+# example walters_stuff/search/topics/
+# basket/search/controller_name_for_zoom_class/ ->
+# search from zoom_class as displayed tab
+# SEE SEARCH RESULTS PATTERN AT END
+
+# example walters_stuff/all/topics/contributed_by/user/7/
+# basket/all/controller_name_for_zoom_class/contributed_by/user/id ->
+# all contributions in a basket for a user with the zoom_class being the default tab
+
+# example site/search/web_links/contributed_by/user/7/for/tramping
+# basket/search/controller_name_for_zoom_class/contributed_by/user/id/for/search_term ->
+# search within contributions in a basket for a user with zom_class being the current tab
+
+# example walters_stuff/all/topics/related_to/image/6/
+# basket/all/controller_name_for_zoom_class/related_to/controller_name_for_zoom_class_singular_for_related_to_item/id ->
+# all related items in a basket for an item with the zoom_class being the default tab
+
+# example site/search/web_links/related_to/image/6/for/ruby
+# basket/search/controller_name_for_zoom_class/related_to/controller_name_for_zoom_class_singular_for_related_to_item/id/for/search_erm ->
+# search related items in a basket for an item with the zoom_class being the default tab
+
+# search results pattern:
+# example site/search/topics/for/gertrude_stein
+# basket/search/controller_name_for_zoom_class/for/search_term
+
+# example site/search/topics/for/gertrude_stein_and_picasso
+# basket/search/controller_name_for_zoom_class/for/search_term_1_boolean_operator_search_term_2
+
+# may want to add a route something like this:
+# basket/search/everything/for/search_term
+# but limit it to rss only
+
+# tech notes:
+# need to create a "for" action in our search controller
+# what is necessary for rss urls?
+
