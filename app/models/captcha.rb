812ff796f31ddd5123b872c7a21b681bb212f758
bob <bob@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-02-19 23:13:33 +0000

    Adding in new files for security image check
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@415 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ app/models/captcha.rb
@@ -0,0 +1,2 @@
+class Captcha < ActiveRecord::Base
+end
---
+++ db/migrate/028_captcha.rb
@@ -0,0 +1,26 @@
+class Captcha < ActiveRecord::Migration
+  def self.up
+    create_table :captchas do |t|
+      t.column :text,       :string,    :limit => 25, :null => false
+      t.column :imageblob,  :binary,           :null => false
+      t.column :created_at, :timestamp,               :null => false
+    end
+  end
+
+  def self.down
+    drop_table :captchas
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
