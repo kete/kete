c2915c28fd4213cdf25085b66f9f840248bf5dfc
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-02-28 04:42:45 +0000

    pulling out dupe code
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@449 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -17,40 +17,4 @@ module TopicsHelper
     result << "</select>\n"
     return result
   end
-
-  def display_xml_attributes(topic)
-    html_string = ""
-    # TODO: these should have their order match the specified order for the topic_type
-    topic.xml_attributes.each do |field_key, field_value|
-      # we now handle multiples
-      multi_re = Regexp.new("_multiple$")
-      if multi_re.match(field_key)
-        # value is going to be a hash like this:
-        # "1" => {field_name => value}, "2" => ...
-        # we want the first field name followed by a :
-        # and all values, separated by spaces (for now)
-        field_name = String.new
-        field_values = Array.new
-        field_value.keys.each do |subfield_key|
-          field_hash = topic.xml_attributes[field_key][subfield_key]
-          field_hash.keys.each do |key|
-            if field_name.blank?
-              field_name = key.humanize
-            end
-            if !field_hash[key].blank? && !field_hash[key].to_s.match("xml_element_name")
-              field_values << field_hash[key]
-            end
-          end
-        end
-        html_string += "<p> #{field_name}: #{field_values.to_sentence} </p>\n"
-      else
-        html_string += "<p> #{field_key.humanize}: "
-        if !field_value.is_a?(Hash) && !field_value.blank?
-          html_string += field_value
-        end
-        html_string += " </p>\n"
-      end
-    end
-    return html_string
-  end
 end
