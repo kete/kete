777b469e4e21e3498b917963bbc7cce2be912747
bob <bob@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-03-01 07:04:59 +0000

    adding in new columns
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@460 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ db/migrate/032_add_extended_fields_example_and_ftype.rb
@@ -0,0 +1,10 @@
+class AddExtendedFieldsExampleAndFtype < ActiveRecord::Migration
+  def self.up
+    add_column :extended_fields, :example, :string, :limit => 255
+    add_column :extended_fields, :ftype,   :string, :limit => 10, :default => 'text'
+  end
+  def self.down
+    remove_column :extended_fields, :example
+    remove_column :extended_fields, :ftype    
+  end
+end
