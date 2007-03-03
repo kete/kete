95d08643553b119c799283b423203b2937358149
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-03-03 01:30:59 +0000

    ticket #72 tried a work around, but in the end, had to limit valid punctuation in label of extended field
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@482 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ lib/extended_fields_helpers.rb
@@ -0,0 +1,17 @@
+module ExtendedFieldsHelpers
+  unless included_modules.include? ExtendedFieldsHelpers
+    # move any problematic punctuation that will mess up our xml
+    # element name to html entities
+    # currently for \,/,&,',",comma,<,>
+    # allowed: .,_,-
+    # should handle the case where & is a part of an entity already
+    # and not escape it
+    def encode_problematic_punctuation_to_entities(string)
+      string.to_s.gsub(/&(?![\#\d\w]+;)/, "&amp;").gsub(/\"/, "&quot;").gsub(/>/, "&gt;").gsub(/</, "&lt;").gsub(/\'/, "&#39;").gsub(/\//, "&#47;").gsub(/\\/, "&#92;").gsub(/,/, "&#44;")
+    end
+
+    def decode_problematic_punctuation_to_entities(string)
+      string.to_s.gsub(/&quot;/, "\"").gsub(/&gt;/, ">").gsub(/&lt;/, "<").gsub(/&#39;/, "\'").gsub(/&#47;/, "/").gsub(/&#92;/, "\\").gsub(/&#44;/, ",").gsub(/&amp;/, "&")
+    end
+  end
+end
