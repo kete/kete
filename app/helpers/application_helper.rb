86106f33814d1789d62493fef5d246aac7af3e60
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-03-03 03:23:09 +0000

    ticket #94 fixed display order to reflect field order set by admin, with the proviso that order is cached locally in the item
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@483 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -410,57 +410,65 @@ module ApplicationHelper
   #---- related to extended_fields for either topic_types or content_types
   def display_xml_attributes(item)
     html_string = String.new
-    # TODO: these should have their order match the specified order for the item_type
+
     if item.xml_attributes
-      item.xml_attributes.each do |field_key, field_value|
-        logger.debug("what is field_key: #{field_key.to_s}")
-        logger.debug("what is field_value: #{field_value.to_s}")
-        # we now handle multiples
-        multi_re = Regexp.new("_multiple$")
-        if multi_re.match(field_key)
-          # value is going to be a hash like this:
-          # "1" => {field_name => value}, "2" => ...
-          # we want the first field name followed by a :
-          # and all values, separated by spaces (for now)
-          field_name = String.new
-          field_values = Array.new
-          field_value.keys.each do |subfield_key|
-            field_hash = item.xml_attributes[field_key][subfield_key]
-            field_hash.keys.each do |key|
-              if field_name.blank?
-                field_name = key.humanize
-              end
-              if !field_hash[key].blank? && !field_hash[key].to_s.match("xml_element_name")
-                field_values << field_hash[key]
+      # the outermost hash is keyed by the field's position
+      # i.e. "1" => {"some_field" => "some_field_value"}, "2" =>...
+      # so you have to go down one to get the actual fields
+      item_xml_hash = item.xml_attributes
+      item_xml_hash.keys.each do |field_position|
+        logger.debug("what is field_position: #{field_position.to_s}")
+        subhash = item_xml_hash[field_position]
+        subhash.each do |field_key, field_value|
+          logger.debug("what is field_key: #{field_key.to_s}")
+          logger.debug("what is field_value: #{field_value.to_s}")
+          # we now handle multiples
+          multi_re = Regexp.new("_multiple$")
+          if multi_re.match(field_key)
+            # value is going to be a hash like this:
+            # "1" => {field_name => value}, "2" => ...
+            # we want the first field name followed by a :
+            # and all values, separated by spaces (for now)
+            field_name = String.new
+            field_values = Array.new
+            field_value.keys.each do |subfield_key|
+              field_hash = item_xml_hash[field_position][field_key][subfield_key]
+              field_hash.keys.each do |key|
+                if field_name.blank?
+                  field_name = key.humanize
+                end
+                if !field_hash[key].blank? && !field_hash[key].to_s.match("xml_element_name")
+                  field_values << field_hash[key]
+                end
               end
             end
-          end
-          if !field_values.to_s.strip.blank?
-            field_value_index = 0
-            field_values.each do |field_value|
+            if !field_values.to_s.strip.blank?
+              field_value_index = 0
+              field_values.each do |field_value|
+                if field_value =~ /^\w+:\/\/[^ ]+/
+                  # this is a url protocal of somesort, make link
+                  field_values[field_value_index] = link_to(field_value,field_value)
+                elsif field_value =~ /^\w+[^ ]*\@\w+\.\w/
+                  field_values[field_value_index] = mail_to(field_value,field_value, :encode => "javascript")
+                else
+                  field_values[field_value_index] = sanitize(field_value)
+                end
+                field_value_index += 1
+              end
+              html_string += "<tr><td class=\"detail-extended-field-label\">#{field_name}:</td><td>#{field_values.to_sentence}</td></tr>\n"
+            end
+          else
+            if !field_value.to_s.strip.blank? && !field_value.is_a?(Hash)
               if field_value =~ /^\w+:\/\/[^ ]+/
                 # this is a url protocal of somesort, make link
-                field_values[field_value_index] = link_to(field_value,field_value)
+                field_value = link_to(field_value,field_value)
               elsif field_value =~ /^\w+[^ ]*\@\w+\.\w/
-                field_values[field_value_index] = mail_to(field_value,field_value, :encode => "javascript")
+                field_value = mail_to(field_value,field_value, :encode => "javascript")
               else
-                field_values[field_value_index] = sanitize(field_value)
+                field_value = sanitize(field_value)
               end
-              field_value_index += 1
+              html_string += "<tr><td class=\"detail-extended-field-label\">#{field_key.humanize}:</td><td>#{field_value}</td></tr>\n"
             end
-            html_string += "<tr><td class=\"detail-extended-field-label\">#{field_name}:</td><td>#{field_values.to_sentence}</td></tr>\n"
-          end
-        else
-          if !field_value.to_s.strip.blank? && !field_value.is_a?(Hash)
-            if field_value =~ /^\w+:\/\/[^ ]+/
-              # this is a url protocal of somesort, make link
-              field_value = link_to(field_value,field_value)
-            elsif field_value =~ /^\w+[^ ]*\@\w+\.\w/
-              field_value = mail_to(field_value,field_value, :encode => "javascript")
-            else
-              field_value = sanitize(field_value)
-            end
-            html_string += "<tr><td class=\"detail-extended-field-label\">#{field_key.humanize}:</td><td>#{field_value}</td></tr>\n"
           end
         end
       end
--- lib/extended_content.rb
+++ lib/extended_content.rb
@@ -1,9 +1,22 @@
+require "rexml/document"
+
+# not much here for now, but could expand later
 module ExtendedContent
-  # not much here for now, but could expand later
+  include REXML
 
   # simply pulls xml attributes in extended_content column out into a hash
   def xml_attributes
-    temp_hash = Hash.from_xml("<dummy_root>#{self.extended_content}</dummy_root>")
-    return temp_hash['dummy_root']
+    # we use rexml for better handling of the order of the hash
+    extended_content = Document.new("<dummy_root>#{self.extended_content}</dummy_root>")
+
+    temp_hash = Hash.new
+    root = extended_content.root
+    position = 1
+    root.elements.each do |field|
+      temp_hash[position.to_s] = Hash.from_xml(field.to_s)
+      position += 1
+    end
+
+    return temp_hash
   end
 end
