5e1e07816cca1fb8268d5d8f9df15ad87c866f9b
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-05-16 04:06:19 +0000

    ticket #122 - added pagination links at the bottom as well as top of results, if there is more than 5 results and more than one page of results
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@634 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,7 +5,8 @@ module SearchHelper
   # with passed with one for passed in zoom_class
   def link_to_zoom_class_results(zoom_class,results_count)
     link_to("#{zoom_class_plural_humanize(zoom_class)} (#{results_count})",
-            :overwrite_params => {:controller_name_for_zoom_class => zoom_class_controller(zoom_class) })
+            :overwrite_params => {:controller_name_for_zoom_class => zoom_class_controller(zoom_class), :page => nil},
+            :trailing_slash => true)
 
   end
 
@@ -27,6 +28,35 @@ module SearchHelper
       return " related to \"#{@source_item.title}\""
     else
       return ''
+    end
+  end
+  def pagination_links(options = { })
+    current_page = options[:current_page]
+    previous_page = options[:previous_page]
+    next_page = options[:next_page]
+    last_page = options[:last_page]
+    placement = options[:placement] || 'top'
+    number_per_page = options[:number_per_page]
+
+    html_string = String.new
+
+    if placement != 'bottom' or (number_per_page > 5 and last_page > 1)
+        html_string += "
+                        <div id=\"prev-next\">"
+
+      if current_page > 1
+        html_string += link_to_previous_page('&lt;&lt; previous',previous_page)
+      else
+        html_string += "&lt;&lt; previous"
+      end
+      html_string += "&nbsp;"
+      if current_page < last_page
+        html_string += link_to_next_page('next &gt;&gt;',next_page)
+      else
+        html_string += "next &gt;&gt;"
+      end
+      html_string += "
+                       </div>"
     end
   end
 end
