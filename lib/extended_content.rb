5e2d1c3d92ab3ab4b3c2b24ceb5a481a12dd02e2
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-05-16 02:51:03 +0000

    ticket #117 - added check for proper order of fields to xml_attributes method, maybe expensive, but is cached after first call to page, also configured extended_fields to be able edit multiple field, was oversight of active_scaffold setup
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@633 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- lib/extended_content.rb
+++ lib/extended_content.rb
@@ -10,9 +10,31 @@ module ExtendedContent
     temp_hash = Hash.new
     root = extended_content.root
     position = 1
-    root.elements.each do |field|
-      temp_hash[position.to_s] = Hash.from_xml(field.to_s)
-      position += 1
+
+    form_fields = Array.new
+    if self.class.name == 'Topic'
+      self.topic_type.self_and_ancestors.each do |topic_type|
+        form_fields = form_fields + topic_type.topic_type_to_field_mappings
+      end
+    else
+      form_fields = ContentType.find_by_class_name(self.class.name).content_type_to_field_mappings
+    end
+
+    if form_fields.size > 0
+      form_fields.each do |extended_field_mapping|
+        f_id = extended_field_mapping.extended_field_label.downcase.gsub(/ /, '_')
+        f_multiple = "#{f_id}_multiple"
+        field_xml = root.elements[f_multiple]
+        # if we didn't match a multiple
+        # then we are all clear to use the plain f_id
+        if field_xml.blank?
+          field_xml = root.elements[f_id]
+        end
+        if !field_xml.blank?
+          temp_hash[position.to_s] = Hash.from_xml(field_xml.to_s)
+          position += 1
+        end
+      end
     end
 
     return temp_hash
