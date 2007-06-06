67d3e080752804a09f39157c4ac685cf86920e60
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-06-06 02:54:58 +0000

    adding my patch for checking format rather than failing silently
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@648 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- vendor/plugins/http_url_validation_improved/lib/http_url_validation_improved.rb
+++ vendor/plugins/http_url_validation_improved/lib/http_url_validation_improved.rb
@@ -8,12 +8,12 @@ module ActiveRecord
 
      # Validates a URL.
      def validates_http_url(*attr_names)
-       configuration = { 
-				:message_not_accessible => "is not accessible",  
-				:message_wrong_content => "is not of the appropriate content type", 
-				:message_moved_permanently => "has moved permanently",
-				:message_url_format => "is not formatted correctly. (Missing 'http://'?)"
-			 }
+       configuration = {
+        :message_not_accessible => "is not accessible",
+        :message_wrong_content => "is not of the appropriate content type",
+        :message_moved_permanently => "has moved permanently",
+        :message_url_format => "is not formatted correctly. (Missing 'http://'?)"
+       }
        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
        validates_each(attr_names, configuration) do |record, attr_name, value|
 
@@ -23,10 +23,10 @@ module ActiveRecord
          end
 
          begin
-					 moved_retry ||= false
-					 not_allowed_retry ||= false
-					 # Check Formatting
-					 raise if not value =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
+           moved_retry ||= false
+           not_allowed_retry ||= false
+           # Check Formatting
+           raise if not value =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
            response = nil
            url = URI.parse(value)
            url.path = "/" if url.path.length < 1
@@ -35,19 +35,19 @@ module ActiveRecord
             http.use_ssl = true
             http.verify_mode = OpenSSL::SSL::VERIFY_NONE
            end
-					 response = not_allowed_retry ? http.request_get(url.path) {|r|} : http.request_head(url.path)
-					 # Comment out as you need to
-					 allowed_codes = [
-						Net::HTTPMovedPermanently,
-						Net::HTTPOK,
-						Net::HTTPCreated,
-						Net::HTTPAccepted,
-						Net::HTTPNonAuthoritativeInformation,
-						Net::HTTPPartialContent,
-						Net::HTTPFound,
-						Net::HTTPTemporaryRedirect,
-						Net::HTTPSeeOther
-					 ]
+           response = not_allowed_retry ? http.request_get(url.path) {|r|} : http.request_head(url.path)
+           # Comment out as you need to
+           allowed_codes = [
+            Net::HTTPMovedPermanently,
+            Net::HTTPOK,
+            Net::HTTPCreated,
+            Net::HTTPAccepted,
+            Net::HTTPNonAuthoritativeInformation,
+            Net::HTTPPartialContent,
+            Net::HTTPFound,
+            Net::HTTPTemporaryRedirect,
+            Net::HTTPSeeOther
+           ]
            # If response is not allowed, raise an error
            raise unless allowed_codes.include?(response.class)
            # Check if the model requires a specific content type
@@ -55,48 +55,53 @@ module ActiveRecord
              record.errors.add(attr_name, configuration[:message_wrong_content]) if response['content-type'].index(configuration[:content_type]).nil?
            end
          rescue
-					# Has the page moved?
-					if response.is_a?(Net::HTTPMovedPermanently)
-					 unless moved_retry
-						moved_retry = true
-						value += "/" # In case webserver is just adding a /
-						retry
-					 else
-						record.errors.add(attr_name, configuration[:message_moved_permanently])
-					 end
-					elsif response.is_a?(Net::HTTPMethodNotAllowed)
-					 unless not_allowed_retry
-						# Retry with a GET
-						not_allowed_retry = true
-						retry
-					 else	
-						record.errors.add(attr_name, configuration[:message_not_accessible]+" (GET method not allowed)")
-					 end
-					else
-						# Just Plain non-accessible
-						record.errors.add(attr_name, configuration[:message_not_accessible]+" "+response.class.to_s)
-					end
+          # Has the page moved?
+          if response.is_a?(Net::HTTPMovedPermanently)
+           unless moved_retry
+            moved_retry = true
+            value += "/" # In case webserver is just adding a /
+            retry
+           else
+            record.errors.add(attr_name, configuration[:message_moved_permanently])
+           end
+          elsif response.is_a?(Net::HTTPMethodNotAllowed)
+           unless not_allowed_retry
+            # Retry with a GET
+            not_allowed_retry = true
+            retry
+           else
+            record.errors.add(attr_name, configuration[:message_not_accessible]+" (GET method not allowed)")
+           end
+          else
+            # if response is nil, then it's a format issue
+            if response.nil?
+              record.errors.add(attr_name, configuration[:message_url_format])
+            else
+              # Just Plain non-accessible
+              record.errors.add(attr_name, configuration[:message_not_accessible]+" "+response.class.to_s)
+            end
+          end
          end
        end
      end
 
-		 def validates_http_domain(*attr_names)
-     	validates_each(attr_names) do |record, attr_name, value|
-			 # Set valid true on successful connect (all we need is one, one is all we need)
-				failed = true
-					possibilities = [value, "www."+value]
-					possibilities.each do |url|
-					begin
-						temp = Socket.gethostbyname(url)
-					rescue SocketError
-						next
-					end
-						failed = false
-						break
-					end
-				record.errors.add(attr_name, "cannot be resolved.") if failed
-		 	end 
-		end
+     def validates_http_domain(*attr_names)
+      validates_each(attr_names) do |record, attr_name, value|
+       # Set valid true on successful connect (all we need is one, one is all we need)
+        failed = true
+          possibilities = [value, "www."+value]
+          possibilities.each do |url|
+          begin
+            temp = Socket.gethostbyname(url)
+          rescue SocketError
+            next
+          end
+            failed = false
+            break
+          end
+        record.errors.add(attr_name, "cannot be resolved.") if failed
+      end
+    end
    end
  end
 end
