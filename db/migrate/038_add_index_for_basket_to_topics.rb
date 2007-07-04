1e5b4e781de7c56bbebc9c96b6a6071543e93867
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-07-04 22:59:43 +0000

    workaround for remove_foreign_key bug
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@734 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/038_add_index_for_basket_to_topics.rb
+++ db/migrate/038_add_index_for_basket_to_topics.rb
@@ -1,11 +1,14 @@
 class AddIndexForBasketToTopics < ActiveRecord::Migration
   def self.up
-    add_column :topics, :index_for_basket_id, :integer, :references => :baskets
+    # TODO: add back in foreign key constraint
+    # after the remove_foreign_key bug is fixed
+    # add_column :topics, :index_for_basket_id, :integer, :references => :baskets
+    add_column :topics, :index_for_basket_id, :integer, :references => nil
     add_column :topic_versions, :index_for_basket_id, :integer, :references => nil
   end
 
   def self.down
-    remove_foreign_key :topics, :index_for_basket_id
+    # remove_foreign_key :topics, :index_for_basket_id
     remove_column :topic_versions, :index_for_basket_id
     remove_column :topics, :index_for_basket_id
   end
