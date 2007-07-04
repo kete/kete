374e94742580ea90b05e73c3ab777bb0457037fc
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-07-04 23:10:31 +0000

    commenting out no longer needed migration step
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@735 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/022_add_default_basket.rb
+++ db/migrate/022_add_default_basket.rb
@@ -1,9 +1,11 @@
 class AddDefaultBasket < ActiveRecord::Migration
   def self.up
-    Basket.create(:name => 'Site', :urlified_name => 'site')
+    # we now rely on bootstrapping for this step
+    # Basket.create(:name => 'Site', :urlified_name => 'site')
   end
 
   def self.down
-    Basket.find(1).destroy
+    # this will be destroyed when we toss baskets table
+    # Basket.find(1).destroy
   end
 end
