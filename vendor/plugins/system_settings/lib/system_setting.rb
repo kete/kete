0115875342bd8a8d1fefd91ec09db155ba76c046
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-07-13 04:09:17 +0000

    adding some naming validation
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@755 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- vendor/plugins/system_settings/lib/system_setting.rb
+++ vendor/plugins/system_settings/lib/system_setting.rb
@@ -1,20 +1,25 @@
 class SystemSetting < ActiveRecord::Base
+  # Walter McGinnis, 2007-07-12
+  # don't allow special characters in name
+  # since will eventually make them into a constant name
+  validates_format_of :name, :with => /^[^\'\"<>\&,\/\\\?]*$/, :message => ": \', \\, /, &, \", ?, <, and > characters aren't allowed"
+
   def self.find_by_name(name)
     super(name.to_s)
   end
-  
+
   def self.[](name)
     find_by_name(name)
   end
-  
+
   def to_f
     value.to_f
   end
-  
+
   def to_i
     value.to_i
   end
-  
+
   def to_s
     value
   end
