32fc03db802a3cbb482b78a217631fb34b0676ad
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-12-05 01:07:46 +0000

    this needs to be the same across restarts and across mongrel instances
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@872 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- config/initializers/protect_forms.rb
+++ config/initializers/protect_forms.rb
@@ -1,4 +1,4 @@
 # Walter McGinnis, 2007-12-03
 # protect forms from receiving attacks
-ActionController::Base.protect_from_forgery :secret => Time.now.to_s
+ActionController::Base.protect_from_forgery :secret => 'kete'
 
