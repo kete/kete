8359783ee8ea18baae3a24c6920549ec1cffa5fb
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-12-16 21:01:30 +0000

    upgrading backgroundrb in process, interim step
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@883 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- vendor/plugins/backgroundrb/specs/data/server/workers/results_parallel_worker.rb
+++
@@ -1,9 +0,0 @@
-class ResultsParallelWorker < BackgrounDRb::Worker::Base
-  def do_work(args)
-    args.times do |counter|
-      results[:data] = Time.now
-      results[:counter] = counter
-    end
-  end
-end
-ResultsParallelWorker.register
