2930e8bd80724e803f871f3d108ba636f8b8322d
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2007-12-28 03:14:50 +0000

    fixed a lingering rails 2.0 scope compatibility issue
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@934 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- app/models/content_type_to_field_mapping.rb
+++ app/models/content_type_to_field_mapping.rb
@@ -6,4 +6,8 @@ class ContentTypeToFieldMapping < ActiveRecord::Base
   acts_as_list :scope => :content_type_id
   piggy_back :extended_field_label_xml_element_name_xsi_type_multiple_and_description,
       :from => :extended_field, :attributes => [:label, :xml_element_name, :xsi_type, :multiple, :description]
+
+  def self.add_as_to(is_required, content_type, field)
+      with_scope(:create => { :required => is_required}) { content_type.concat field }
+  end
 end
