af7ca4b418aecf40014a22ec386bb9d2137e6e3d
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2008-01-14 07:27:00 +0000

    adding new validation plugin written by Katipo for Kete to warn against adding insecure user submitted html
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@983 6ef355b2-2f55-425c-b0e5-54d779d0066e

---
+++ vendor/plugins/validates_as_sanitized_html/init.rb
@@ -0,0 +1,2 @@
+# Include hook code here
+require 'validates_as_sanitized_html'
---
+++ vendor/plugins/validates_as_sanitized_html/lib/validates_as_sanitized_html.rb
@@ -0,0 +1,35 @@
+require 'hpricot'
+# thanks to validates_xml plugin for example code
+# add simple sanitizing and valid html validation
+module ActiveRecord
+  module Validations
+    module ClassMethods
+      def validates_as_sanitized_html(*attr_names)
+        configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid],
+                          :on => :save,
+                          :with => nil }
+        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+        validates_each(attr_names, configuration) do |record, attr_name, value|
+          # allow for turning off sanitization on a record by record basis
+          # via virtual attribute on record
+          do_not_sanitize = !record.do_not_sanitize.nil? && (record.do_not_sanitize.to_i == 1 || record.do_not_sanitize == true) ?  true : false
+          unless do_not_sanitize
+            # TODO: see if we can reuse sanitization
+            # from rail's html/sanitize or helpers/sanitize_helper
+            # very simple check for bad elements
+            if !value.blank?
+              if !value.scan(/<form|<script|<input/).blank?
+                record.errors.add(attr_name,
+                                  ": we aren't currently allowing forms or javascript in submitted HTML for security reasons.")
+              else
+                new_value = Hpricot(value).to_html
+                record.errors.add(attr_name, ": is not valid html.  It looks like you didn't close all your tags.") if new_value != value
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
