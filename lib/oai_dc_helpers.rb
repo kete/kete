79f0909ccb68e91e3361f1b4d34db4b369dca52d
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2008-05-24 02:19:12 +0000

    enhancement: adding in dc:rights to oai record for each item, license if specified by item, site terms and conditions if not
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@1187 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- lib/oai_dc_helpers.rb
+++ lib/oai_dc_helpers.rb
@@ -1,4 +1,6 @@
 # oai dublin core xml helpers
+# TODO: evaluate whether we can simply go with SITE_URL
+# rather than request hacking
 module OaiDcHelpers
   unless included_modules.include? OaiDcHelpers
     def self.included(klass)
@@ -37,24 +39,24 @@ module OaiDcHelpers
         # comments always point back to the thing they are commenting on
         commented_on_item = item.commentable
         uri_attrs = {
-          :controller => zoom_class_controller(commented_on_item.class.name), 
-          :action => 'show', 
-          :id => commented_on_item, 
-          :format => nil, 
-          :urlified_name => commented_on_item.basket.urlified_name, 
-          :anchor => "comment-#{item.id}", 
+          :controller => zoom_class_controller(commented_on_item.class.name),
+          :action => 'show',
+          :id => commented_on_item,
+          :format => nil,
+          :urlified_name => commented_on_item.basket.urlified_name,
+          :anchor => "comment-#{item.id}",
           :private => item.commentable_private?.to_s
         }
       else
-        
-        uri_attrs = { 
-          :controller => zoom_class_controller(item.class.name), 
-          :action => 'show', 
-          :id => item, 
-          :format => nil, 
+
+        uri_attrs = {
+          :controller => zoom_class_controller(item.class.name),
+          :action => 'show',
+          :id => item,
+          :format => nil,
           :urlified_name => item.basket.urlified_name
         }
-        
+
         # Link to private version if generating OAI record for it..
         uri_attrs.merge!({ :private => "true" }) if item.respond_to?(:private) && item.private?
       end
@@ -171,6 +173,15 @@ module OaiDcHelpers
       if !format.blank?
         xml.tag!("dc:format", format)
       end
+    end
+
+    # if there is a license for item, put in its url
+    # otherwise site's terms and conditions url
+    def oai_dc_xml_dc_rights(xml, item)
+      rights = !item.license.blank? ? item.license.url : SITE_URL.chop + url_for(:id => 4,
+                                                                            :urlified_name => Basket.find(ABOUT_BASKET).urlified_name,
+                                                                            :action => 'show' )
+      xml.tag!("dc:rights", rights)
     end
 
     def oai_dc_xml_dc_description_for_file(xml, item, passed_request = nil)
