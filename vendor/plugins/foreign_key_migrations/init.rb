709088c968b44f7d6470d953eca3878ce7e768dc
james <james@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2008-06-30 03:33:05 +0000

    * Removing Rails in preparation for replacent with Rails 2.1 * Removing plugins to be updated with newer versions (changed repository locations)
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@1243 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- vendor/plugins/foreign_key_migrations/init.rb
+++
@@ -1,3 +0,0 @@
-ActiveRecord::Base.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::Base)
-ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::ConnectionAdapters::AbstractAdapter)
-ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::ConnectionAdapters::TableDefinition)
