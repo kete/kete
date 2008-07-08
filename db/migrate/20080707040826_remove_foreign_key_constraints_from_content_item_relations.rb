28274f81b6076582f101c8cd30e56ffdd7201d83
walter <walter@6ef355b2-2f55-425c-b0e5-54d779d0066e>
2008-07-08 23:42:27 +0000

    adding clarifying comments to last commit of migration that removes fk constraints on content_item_relations
    git-svn-id: http://svn.kete.net.nz/projects/kete/trunk@1306 6ef355b2-2f55-425c-b0e5-54d779d0066e

--- db/migrate/20080707040826_remove_foreign_key_constraints_from_content_item_relations.rb
+++ db/migrate/20080707040826_remove_foreign_key_constraints_from_content_item_relations.rb
@@ -1,9 +1,9 @@
 class RemoveForeignKeyConstraintsFromContentItemRelations < ActiveRecord::Migration
   def self.up
-    # foreign_key_migrations does not appear to support removing foreign key constaints 
-    # without completely recreating the column, which we need to avoid. So, as a work 
-    # around, we're doing to remove the foreign keys by hand.
-    
+    # we're rolling our own here because we have had some problems with
+    # foreign_key_migrations support of removing foreign key constaints
+    # IMPORTANT: it should be noted that there are some assumptions here about the constraint name
+    # and that this may not work with PostgreSQL or other RDBMSs, please feel free to submit patches!
     remove_foreign_key_constraint "content_item_relations", "content_item_relations_ibfk_1"
     remove_foreign_key_constraint "deleted_content_item_relations", "deleted_content_item_relations_ibfk_1"
   end
@@ -12,19 +12,19 @@ class RemoveForeignKeyConstraintsFromContentItemRelations < ActiveRecord::Migrat
     add_foreign_key_constraint "content_item_relations", "content_item_relations_ibfk_1", "topics"
     add_foreign_key_constraint "deleted_content_item_relations", "deleted_content_item_relations_ibfk_1", "topics"
   end
-  
+
   class << self
-  
+
     def add_foreign_key_constraint(table_name, symbol, foreign_table, foreign_primary_key = "id")
       execute("ALTER TABLE #{table_name} ADD CONSTRAINT `#{symbol}` FOREIGN KEY `#{foreign_table.singularize}_#{foreign_primary_key}` (`#{foreign_table.singularize}_#{foreign_primary_key}`) REFERENCES `#{foreign_table}` (`#{foreign_primary_key}`)")
       print "Adding foreign key constraint with key \"#{symbol}\" on column #{foreign_table}_#{foreign_primary_key}.\n"
     end
-  
+
     def remove_foreign_key_constraint(table_name, symbol)
       execute("ALTER TABLE #{table_name} DROP FOREIGN KEY #{symbol}")
       print "Removing foreign key constraint with key \"#{symbol}\".\n"
     end
 
   end
-    
+
 end
