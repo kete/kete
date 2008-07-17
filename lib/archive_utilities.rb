5e7ea68e5d4b58487ac80a8489c5048e30e570a5
Kieran Pilkington <kieran@katipo.co.nz>
2008-07-17 17:45:32 +1200

    bugfix: missed a place where the it checks arhive mime types to decide how to extract something

--- lib/archive_utilities.rb
+++ lib/archive_utilities.rb
@@ -4,11 +4,11 @@ module ArchiveUtilities
     # good thing we don't officially support it!
     def decompress_under(target_directory)
       case content_type
-      when 'application/zip'
+      when 'application/zip', 'application/x-zip', 'application/x-zip-compressed'
         `unzip #{self.full_filename} -d #{target_directory}`
       when 'application/x-gtar', 'application/x-tar'
         `tar xf #{self.full_filename} #{target_directory}`
-      when 'application/x-gzip'
+      when 'application/x-gzip', 'application/x-compressed-tar'
         if !self.filename.scan("tgz").blank? or !self.filename.scan("tar\.gz").blank?
           `tar xfz #{self.full_filename} -C #{target_directory}`
         else
