660d22ee133fe9d9bdb6c7ee4f8dda3598620a07
Kieran Pilkington <kieran@katipo.co.nz>
2008-08-12 16:15:49 +1200

    refinement: removed duplication, more flexible if the values change

--- test/unit/pqf_query_test.rb
+++ test/unit/pqf_query_test.rb
@@ -6,6 +6,14 @@ class PqfQueryTest < ActiveSupport::TestCase
     @pqf_query = PqfQuery.new
   end
 
+  def set_short_consts
+    @qas = PqfQuery::QUALIFYING_ATTRIBUTE_SPECS
+    @as = PqfQuery::ATTRIBUTE_SPECS
+    @dts = PqfQuery::DATETIME_SPECS
+    @dtcs = PqfQuery::DATETIME_COMPARISON_SPECS
+    @dnadimf = PqfQuery::DO_NOT_AUTO_DEF_INCLUDE_METHODS_FOR
+  end
+
   def test_constants_are_defined
     assert_equal "constant", defined?(PqfQuery::QUALIFYING_ATTRIBUTE_SPECS)
     assert_equal "constant", defined?(PqfQuery::ATTRIBUTE_SPECS)
@@ -15,6 +23,8 @@ class PqfQueryTest < ActiveSupport::TestCase
   end
 
   def test_constants_have_right_data
+    set_short_consts # saves us typing long constant names
+
     qualifying_attribute_specs = {
       'relevance' => "@attr 2=102 @attr 5=3 @attr 5=103 ",
       'exact' => "@attr 4=3 ",
@@ -26,7 +36,7 @@ class PqfQueryTest < ActiveSupport::TestCase
       'gt' => "@attr 2=5 ",
       'sort_stub' => "@attr 7="
     }
-    assert_equal qualifying_attribute_specs, PqfQuery::QUALIFYING_ATTRIBUTE_SPECS
+    assert_equal qualifying_attribute_specs, @qas
 
     attribute_specs = {
       'oai_identifier' => "@attr 1=12 ",
@@ -42,14 +52,14 @@ class PqfQueryTest < ActiveSupport::TestCase
       'last_modified_sort' => "@attr 1=1012 ",
       'date_sort' => "@attr 1=30 "
     }
-    assert_equal attribute_specs, PqfQuery::ATTRIBUTE_SPECS
+    assert_equal attribute_specs, @as
 
     datetime_specs = {
       'oai_datestamp' => "@attr 1=1012 @attr 4=5 ",
       'last_modified' => "@attr 1=1012 @attr 4=5 ",
       'date' => "@attr 1=30 @attr 4=5 "
     }
-    assert_equal datetime_specs, PqfQuery::DATETIME_SPECS
+    assert_equal datetime_specs, @dts
 
     datetime_comparison_specs = {
       'before' => "@attr 2=1 ",
@@ -58,16 +68,18 @@ class PqfQueryTest < ActiveSupport::TestCase
       'on_or_before' => "@attr 2=2 ",
       'on_or_after' => "@attr 2=4 "
     }
-    assert_equal datetime_comparison_specs, PqfQuery::DATETIME_COMPARISON_SPECS
+    assert_equal datetime_comparison_specs, @dtcs
 
     do_not_auto_def_include_methods_for = ["date_sort", "last_modified_sort"]
-    assert_equal do_not_auto_def_include_methods_for, PqfQuery::DO_NOT_AUTO_DEF_INCLUDE_METHODS_FOR
+    assert_equal do_not_auto_def_include_methods_for, @dnadimf
   end
 
   def test_to_string_and_add_web_link_specific_query
+    set_short_consts # saves us typing long constant names
+
     assert_equal "  ", @pqf_query.to_s
     @pqf_query.add_web_link_specific_query
-    assert_equal "@or   @attr 1=21  ", @pqf_query.to_s
+    assert_equal "@or   #{@as['subjects']} ", @pqf_query.to_s
   end
 
   def test_correct_attribute_spec_methods_defined
@@ -91,9 +103,11 @@ class PqfQueryTest < ActiveSupport::TestCase
   end
 
   def test_exact_match_for_part_of_oai_identifier
-    assert_equal "@attr 1=12 \":Topic:\"", @pqf_query.exact_match_for_part_of_oai_identifier("topics".classify, :operator => 'none')
-    assert_equal "@attr 1=12 \":site:\"", @pqf_query.exact_match_for_part_of_oai_identifier(Basket.first.urlified_name)
-    assert_equal "@attr 1=12 @or \":site:\" \":documentation:\"", @pqf_query.exact_match_for_part_of_oai_identifier([Basket.first.urlified_name, Basket.last.urlified_name])
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "#{@as['oai_identifier']}\":Topic:\"", @pqf_query.exact_match_for_part_of_oai_identifier("topics".classify, :operator => 'none')
+    assert_equal "#{@as['oai_identifier']}\":site:\"", @pqf_query.exact_match_for_part_of_oai_identifier(Basket.first.urlified_name)
+    assert_equal "#{@as['oai_identifier']}@or \":site:\" \":documentation:\"", @pqf_query.exact_match_for_part_of_oai_identifier([Basket.first.urlified_name, Basket.last.urlified_name])
   end
 
   def test_datetime_spec_methods_defined
@@ -115,35 +129,47 @@ class PqfQueryTest < ActiveSupport::TestCase
   end
 
   def test_oai_datestamp_between
-    assert_equal "@and @attr 2=4 @attr 1=1012 @attr 4=5 \"2008-01-01 00:00:00\" @attr 2=2 @attr 1=1012 @attr 4=5 \"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_between({:beginning => '2008-01-01 00:00:00', :ending => '2008-12-31 23:59:59', :only_return_as_string => true})
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "@and #{@dtcs['on_or_after']}#{@dts['oai_datestamp']}\"2008-01-01 00:00:00\" #{@dtcs['on_or_before']}#{@dts['oai_datestamp']}\"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_between({:beginning => '2008-01-01 00:00:00', :ending => '2008-12-31 23:59:59', :only_return_as_string => true})
   end
 
   def test_oai_datestamp_comparison
-    assert_equal "@and @attr 2=4 @attr 1=1012 @attr 4=5 \"2008-01-01 00:00:00\" @attr 2=2 @attr 1=1012 @attr 4=5 \"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_comparison({:beginning => '2008-01-01 00:00:00', :ending => '2008-12-31 23:59:59', :only_return_as_string => true})
-    assert_equal "@attr 2=4 @attr 1=1012 @attr 4=5 \"2008-01-01 00:00:00\"", @pqf_query.oai_datestamp_comparison({:beginning => '2008-01-01 00:00:00', :only_return_as_string => true})
-    assert_equal "@attr 2=2 @attr 1=1012 @attr 4=5 \"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_comparison({:ending => '2008-12-31 23:59:59', :only_return_as_string => true})
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "@and #{@dtcs['on_or_after']}#{@as['last_modified']}\"2008-01-01 00:00:00\" #{@dtcs['on_or_before']}#{@as['last_modified']}\"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_comparison({:beginning => '2008-01-01 00:00:00', :ending => '2008-12-31 23:59:59', :only_return_as_string => true})
+    assert_equal "#{@dtcs['on_or_after']}#{@as['last_modified']}\"2008-01-01 00:00:00\"", @pqf_query.oai_datestamp_comparison({:beginning => '2008-01-01 00:00:00', :only_return_as_string => true})
+    assert_equal "#{@dtcs['on_or_before']}#{@as['last_modified']}\"2008-12-31 23:59:59\"", @pqf_query.oai_datestamp_comparison({:ending => '2008-12-31 23:59:59', :only_return_as_string => true})
   end
 
   def test_creators_or_contributors_include
-    assert_equal "@or @attr 1=1003 \"admin\" @attr 1=1020 \"admin\"", @pqf_query.creators_or_contributors_include("admin", {:only_return_as_string => true})
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "@or #{@as['creators']}\"admin\" #{@as['contributors']}\"admin\"", @pqf_query.creators_or_contributors_include("admin", {:only_return_as_string => true})
   end
 
   def test_title_or_any_text_includes
-    assert_equal "@attr 2=102 @attr 5=3 @attr 5=103 ", @pqf_query.title_or_any_text_includes("")
-    assert_equal "@attr 2=102 @attr 5=3 @attr 5=103 @or @attr 1=4   \"One\"  @attr 1=1016   \"One\"  ", @pqf_query.title_or_any_text_includes("One")
-    assert_equal "@attr 2=102 @attr 5=3 @attr 5=103 @or @attr 1=4  @and  \"One\" \"Two\" @attr 1=1016  @and  \"One\" \"Two\" ", @pqf_query.title_or_any_text_includes("One Two")
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "#{@qas['relevance']}", @pqf_query.title_or_any_text_includes("")
+    assert_equal "#{@qas['relevance']}@or #{@as['title']}  \"One\"  #{@as['any_text']}  \"One\"  ", @pqf_query.title_or_any_text_includes("One")
+    assert_equal "#{@qas['relevance']}@or #{@as['title']} @and  \"One\" \"Two\" #{@as['any_text']} @and  \"One\" \"Two\" ", @pqf_query.title_or_any_text_includes("One Two")
   end
 
   def test_methods_should_have_aliases
-    assert_equal "@attr 1=12 \":Topic:\"", @pqf_query.kind_is("topics".classify, :operator => 'none')
-    assert_equal "@attr 1=12 \":site:\"", @pqf_query.within(Basket.first.urlified_name)
+    set_short_consts # saves us typing long constant names
+
+    assert_equal "#{@as['oai_identifier']}\":Topic:\"", @pqf_query.kind_is("topics".classify, :operator => 'none')
+    assert_equal "#{@as['oai_identifier']}\":site:\"", @pqf_query.within(Basket.first.urlified_name)
   end
 
   def test_push_to_appropriate_variables
+    set_short_consts # saves us typing long constant names
+
     @pqf_query.title_or_any_text_includes("One Two")
     @pqf_query.oai_datestamp_comparison({:beginning => '2008-01-01 00:00:00', :ending => '2008-12-31 23:59:59'})
     @pqf_query.creators_or_contributors_include("admin")
-    expect = "@and @and @attr 2=102 @attr 5=3 @attr 5=103 @or @attr 1=4  @and  \"One\" \"Two\" @attr 1=1016  @and  \"One\" \"Two\"  @and @attr 2=4 @attr 1=1012 @attr 4=5 \"2008-01-01 00:00:00\" @attr 2=2 @attr 1=1012 @attr 4=5 \"2008-12-31 23:59:59\" @or @attr 1=1003 \"admin\" @attr 1=1020 \"admin\" "
+    expect = "@and @and #{@qas['relevance']}@or #{@as['title']} @and  \"One\" \"Two\" #{@as['any_text']} @and  \"One\" \"Two\"  @and #{@dtcs['on_or_after']}#{@as['last_modified']}\"2008-01-01 00:00:00\" #{@dtcs['on_or_before']}#{@as['last_modified']}\"2008-12-31 23:59:59\" @or #{@as['creators']}\"admin\" #{@as['contributors']}\"admin\" "
     assert_equal expect, @pqf_query.to_s
   end
 end
