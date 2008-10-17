7d75d0bfb5648e4d7e64b8036486e4b3eb2634dc
Kieran Pilkington <kieran@katipo.co.nz>
2008-10-17 19:44:18 +1300

    bugfix: fixing a few tests that were failing after all the branches were merged in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,8 @@ module ApplicationHelper
 
   def page_description
     return DEFAULT_PAGE_DESCRIPTION if current_item.nil?
-    description_text = current_item.short_summary.blank? ? current_item.description : current_item.short_summary
+    description_text = (current_item.respond_to?(:short_summary) && !current_item.short_summary.blank?) ? current_item.short_summary : current_item.description
+    return DEFAULT_PAGE_DESCRIPTION if description_text.blank?
     strip_tags(truncate(description_text, 180)).gsub("\"", "").squish
   end
 
