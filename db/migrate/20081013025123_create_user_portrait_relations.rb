4feaf6ea1dbf21758814ee2880dac817d6f0bb05
Walter McGinnis <walter@katipo.co.nz>
2008-11-14 17:25:41 +1300

    refinement: same as last commit, DRY up migration

--- db/migrate/20081013025123_create_user_portrait_relations.rb
+++ db/migrate/20081013025123_create_user_portrait_relations.rb
@@ -1,9 +1,7 @@
 class CreateUserPortraitRelations < ActiveRecord::Migration
   def self.up
     create_table :user_portrait_relations do |t|
-      t.integer :position
-      t.integer :user_id
-      t.integer :still_image_id
+      t.integer :position, :user_id, :still_image_id
 
       t.timestamps
     end
