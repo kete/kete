0f615d2d42a00e1219102dc597023dc41bf2c701
Kieran Pilkington <kieran@katipo.co.nz>
2008-11-18 11:07:55 +1300

    bugfix: a very simple fix for restarting feed workers when bgrb goes offline. It'll load them all on bgrb startup, then kill the worker that loaded them.

---
+++ lib/workers/load_feed_updaters_worker.rb
@@ -0,0 +1,15 @@
+class LoadFeedUpdatersWorker < BackgrounDRb::MetaWorker
+  set_worker_name :load_feed_updaters_worker
+
+  # this worker is run once during backgroundrb startup to restart all Feed workers
+
+  def create(args = nil)
+    Feed.all.each do |feed|
+      MiddleMan.new_worker( :worker => :feeds_worker, :worker_key => feed.to_worker_key, :data => feed.id )
+    end
+
+    exit # stop this worker
+  end
+
+end
+
