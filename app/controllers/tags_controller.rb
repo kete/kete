fd4f812e052619fdc7260e0912a24301ad7ef8c0
Kieran Pilkington <kieran@katipo.co.nz>
2008-11-17 17:19:26 +1300

    refinement: use current basket tag sorting options if user submitted values arn't supplied, supply a 'no tags' type message if no tags, removed option to sort by tag cloud, use categories unless overridden in basket edit

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @type = params[:type] || 'tag_cloud'
-    @order = params[:order] || 'random'
+    @type = @current_basket.index_page_tags_as || 'tag cloud'
+    @order = params[:order] || @current_basket.index_page_order_tags_by || 'random'
     @tags_in_reverse = (params[:tags_in_reverse] && params[:tags_in_reverse] == 'reverse') ? true : false
-    @current_page = (params[:page] && params[:page].to_i > 0) ? params[:page].to_i : 1
 
+    @current_page = (params[:page] && params[:page].to_i > 0) ? params[:page].to_i : 1
     @number_per_page = 25
 
     @tag_counts_array = @current_basket.tag_counts_array({:limit => false, :order => @order, :tags_in_reverse => @tags_in_reverse})
