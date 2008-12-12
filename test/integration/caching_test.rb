67456824f1a69f501bfe1fb372703990fb9b29e9
Kieran Pilkington <kieran@katipo.co.nz>
2008-12-13 12:56:34 +1300

    tests: finishing of the homepage cache tests (takes into account homepage topic, realted topics, available private version and automatic (default) private version from different user role levels

--- test/integration/caching_test.rb
+++ test/integration/caching_test.rb
@@ -15,6 +15,8 @@ class CachingTest < ActionController::IntegrationTest
       @@cache_basket.save
 
       add_admin_as_super_user
+      add_joe_as_member_to(@@cache_basket)
+      add_john
       login_as('admin')
     end
 
@@ -148,23 +150,88 @@ class CachingTest < ActionController::IntegrationTest
 
     # Test cases past this point are not yet implemented
 
-    context "when it has a private version" do
+    context "when privacy controls are enabled" do
 
-      should "cache seperate privacies" do
+      setup do
+        @@cache_basket.show_privacy_controls = true
+        @@cache_basket.save
       end
 
-      should "not be shown to the wrong users" do
+      teardown do
+        @@cache_basket.show_privacy_controls = false
+        @@cache_basket.save
       end
 
-      context "and when basket has private as default privacy" do
+      context "and when homepage has a private version" do
 
-        should "not show private homepage when not allowed" do
+        setup do
+          @topic = new_item(@@cache_basket, 'Topic', true, 'Public Title', 'Public Description') do |field_prefix|
+            choose "#{field_prefix}_private_false"
+          end
+          check_homepage_current(@topic)
+          @topic = update_item(@topic, 'Private Title', 'Private Description') do |field_prefix|
+            choose "#{field_prefix}_private_true"
+          end
         end
 
-        should "show the private homepage when allowed" do
+        should "cache seperate privacies" do
+          check_private_version_viewable(@topic)
         end
 
-        should "not be shown to the wrong users" do
+        should "not link to private version unless user has permission" do
+          check_private_version_viewable(@topic) # as admin
+          logout
+          check_private_version_not_viewable(@topic, { :check_edit_links => false })
+          login_as('john')
+          check_private_version_not_viewable(@topic, { :check_edit_links => false })
+          login_as('joe')
+          check_private_version_viewable(@topic)
+        end
+
+        should "default to public version when non member tries to access private version" do
+          visit "/#{@topic.basket.urlified_name}/index_page?private=true"
+          check_private_version_viewable(@topic, { :on_private_homepage_already => true }) # as admin
+          logout
+          visit "/#{@topic.basket.urlified_name}/index_page?private=true"
+          check_private_version_not_viewable(@topic, { :on_private_homepage_already => true,
+                                                       :check_edit_links => false })
+          login_as('john')
+          visit "/#{@topic.basket.urlified_name}/index_page?private=true"
+          check_private_version_not_viewable(@topic, { :on_private_homepage_already => true,
+                                                       :check_edit_links => false })
+          login_as('joe')
+          visit "/#{@topic.basket.urlified_name}/index_page?private=true"
+          check_private_version_viewable(@topic, { :on_private_homepage_already => true })
+        end
+
+        context "and when basket has private as default privacy" do
+
+          setup do
+            @@cache_basket.private_default = true
+            @@cache_basket.save
+          end
+
+          teardown do
+            @@cache_basket.private_default = false
+            @@cache_basket.save
+          end
+
+          should "show private homepage automatically unless user is less than member" do
+            visit "/#{@topic.basket.urlified_name}"
+            check_private_version_viewable(@topic, { :on_private_homepage_already => true }) # as admin
+            logout
+            visit "/#{@topic.basket.urlified_name}"
+            check_private_version_not_viewable(@topic, { :on_private_homepage_already => true,
+                                                         :check_edit_links => false })
+            login_as('john')
+            visit "/#{@topic.basket.urlified_name}"
+            check_private_version_not_viewable(@topic, { :on_private_homepage_already => true,
+                                                         :check_edit_links => false })
+            login_as('joe')
+            visit "/#{@topic.basket.urlified_name}"
+            check_private_version_viewable(@topic, { :on_private_homepage_already => true })
+          end
+
         end
 
       end
@@ -175,20 +242,71 @@ class CachingTest < ActionController::IntegrationTest
 
   private
 
-  def check_homepage_current(item)
+  def check_homepage_current(item, args = {})
+    # I tried args[:check_edit_links] || true
+    # But it looks like Ruby assigns it true if the args is false (as well as nil)
+    # So we have to do a ternary operator to assign it instead
+    on_homepage_already = !args[:on_homepage_already].nil? ? args[:on_homepage_already] : false
+    check_edit_links = !args[:check_edit_links].nil? ? args[:check_edit_links] : true
+    check_should_not = !args[:check_should_not].nil? ? args[:check_should_not] : false
     controller = zoom_class_controller(item.class.name)
-    visit "/#{item.basket.urlified_name}"
-    #body_should_contain item.title
-    body_should_contain item.description
-    body_should_contain "/#{item.basket.urlified_name}/#{controller}/show/#{item.to_param}"
-    body_should_contain "/#{item.basket.urlified_name}/#{controller}/edit/#{item.to_param}"
-    body_should_contain "/#{item.basket.urlified_name}/#{controller}/history/#{item.to_param}"
+    visit "/#{item.basket.urlified_name}" unless on_homepage_already
+    item_id = item.to_param #item.private? ? item.id : item.to_param
+    contents = [item.description]
+    if check_edit_links
+      contents << "/#{item.basket.urlified_name}/#{controller}/show/#{item_id}"
+      contents << "/#{item.basket.urlified_name}/#{controller}/edit/#{item_id}"
+      contents << "/#{item.basket.urlified_name}/#{controller}/history/#{item_id}"
+    end
+    contents.each do |content|
+      check_should_not ? body_should_not_contain(content) : body_should_contain(content)
+    end
   end
 
   def check_recent_topics_include(item)
     visit "/#{item.basket.urlified_name}"
     body_should_contain item.title
     body_should_contain item.description
+  end
+
+  def check_private_version_viewable(item, args = {})
+    # I tried args[:check_edit_links] || true
+    # But it looks like Ruby assigns it true if the args is false (as well as nil)
+    # So we have to do a ternary operator to assign it instead
+    on_private_homepage_already = !args[:on_private_homepage_already].nil? ? args[:on_private_homepage_already] : false
+    check_edit_links = !args[:check_edit_links].nil? ? args[:check_edit_links] : true
+    unless on_private_homepage_already
+      visit "/#{item.basket.urlified_name}"
+      body_should_contain "Private version"
+      click_link "Private Version"
+    end
+    check_homepage_current(item, { :on_homepage_already => true,
+                                   :check_edit_links => check_edit_links,
+                                   :check_should_not => true })
+    item.private_version! if item.has_private_version?
+    check_homepage_current(item, { :on_homepage_already => true,
+                                   :check_edit_links => check_edit_links })
+    body_should_contain "Public version (live)"
+    item.public_version! if item.is_private? # make sure we revert it back to public version for the next test
+  end
+
+  def check_private_version_not_viewable(item, args = {})
+    # I tried args[:check_edit_links] || true
+    # But it looks like Ruby assigns it true if the args is false (as well as nil)
+    # So we have to do a ternary operator to assign it instead
+    on_private_homepage_already = !args[:on_private_homepage_already].nil? ? args[:on_private_homepage_already] : false
+    check_edit_links = !args[:check_edit_links].nil? ? args[:check_edit_links] : true
+    unless on_private_homepage_already
+      visit "/#{item.basket.urlified_name}"
+      body_should_not_contain "Private version"
+    end
+    check_homepage_current(item, { :on_homepage_already => true,
+                                   :check_edit_links => check_edit_links })
+    item.private_version! if item.has_private_version?
+    check_homepage_current(item, { :on_homepage_already => true,
+                                   :check_edit_links => check_edit_links,
+                                   :check_should_not => true })
+    item.public_version! if item.is_private? # make sure we revert it back to public version for the next test
   end
 
 end
