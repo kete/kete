de2bb7d588e9868f101fc5afdc798c4b36c8d300
James Stradling <james@katipo.co.nz>
2008-12-21 15:15:50 +1300

    Added some further basket tests covering the UI for changing basket moderation settings partially.

--- test/integration/basket_test.rb
+++ test/integration/basket_test.rb
@@ -38,6 +38,42 @@ class BasketTest < ActionController::IntegrationTest
       end
 
     end
+    
+    context "A new basket" do
+
+      setup do
+        add_sarah_as_super_user
+        login_as('sarah')
+
+        @basket = new_basket
+      end
+
+      should "be able to turn on full moderation" do
+        
+        # For some reason false != false ??
+        assert_equal "false", @basket.settings[:fully_moderated].to_s
+        
+        visit "/site/baskets/edit/#{@basket.id}"
+        select "moderator views before item approved", :from => "settings_fully_moderated"
+        click_button "Update"
+        body_should_contain "Basket was successfully updated."
+        assert @basket.settings[:fully_moderated]
+      end
+
+      should "be able to turn off full moderation" do
+        @basket.settings[:fully_moderated] = true
+        @basket.save!
+        assert @basket.settings[:fully_moderated]
+        
+        visit "/site/baskets/edit/#{@basket.id}"
+        body_should_contain '<option value="true" selected="selected">moderator views before item approved</option>'
+        select "moderation upon being flagged", :from => "settings_fully_moderated"
+        click_button "Update"
+        body_should_contain "Basket was successfully updated."
+        assert_equal "false", @basket.settings[:fully_moderated].to_s
+      end
+
+    end
 
   end
 
