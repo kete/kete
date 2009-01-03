f5b8f0f9fbe8d2e27f20927ef002c644ece59dee
Walter McGinnis <walter@katipo.co.nz>
2009-01-03 19:29:22 +1300

    outlining tests to be built to cover existing functionality

--- test/integration/related_to_topic_test.rb
+++ test/integration/related_to_topic_test.rb
@@ -24,11 +24,30 @@ class RelatedToTopicTest < ActionController::IntegrationTest
       end
 
       related_classes = ZOOM_CLASSES - %w(Comment)
+
+      # only relevant to related classes that have attachments
+      attachable_classes = related_classes - %w(WebLink Topic)
+
       related_classes.each do |class_name|
-        should "have empty related #{class_name}" do
-          body_should_contain "#{zoom_class_plural_humanize(class_name)} (0)"
+        context "the related #{class_name} subsection" do
+          should "be empty to start" do
+            body_should_contain "#{zoom_class_plural_humanize(class_name)} (0)"
+          end
+
+          should_eventually "be able to create related #{class_name}"
+          should_eventually "be able to link existing related #{class_name}"
+          should_eventually "be able to unlink related #{class_name}"
+          should_eventually "be able to restore unlinked related #{class_name}"
+
+          if attachable_classes.include?(class_name)
+            should_eventually "be able to upload a zip file of related #{class_name.tableize}"
+          end
+
+          should_eventually "not display links to items with no public version"
+
         end
       end
     end
   end
 end
+
