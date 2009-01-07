a99e3520e8bab6f11dce6caa50d8f7fe0dc53fe0
Kieran Pilkington <kieran@katipo.co.nz>
2009-01-07 15:49:36 +1300

    Adding local variable with multiple pages to test http authentication on. All tests passing.

--- test/integration/site_lockdown_auth_test.rb
+++ test/integration/site_lockdown_auth_test.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/integration_test_helper'
 
 class SiteLockdownAuthTest < ActionController::IntegrationTest
 
+  pages = ['/',
+           '/site/all/topics',
+           '/site/baskets/choose_type',
+           '/site/all/topics/rss.xml',
+           '/about/topics/show/1']
+
   context "When a site has http credentials, it" do
 
     setup do
@@ -17,16 +23,20 @@ class SiteLockdownAuthTest < ActionController::IntegrationTest
     end
 
     should "require that the viewer login" do
-      basic_auth 'test', 'test'
-      visit "/"
-      body_should_not_contain 'HTTP Basic: Access denied.'
-      body_should_contain 'Kete'
+      pages.each do |page|
+        basic_auth 'test', 'test'
+        visit page
+        body_should_not_contain 'HTTP Basic: Access denied.'
+        body_should_contain 'Kete'
+      end
     end
 
     should "not allow those with invalid credentials" do
-      visit "/"
-      body_should_not_contain 'Kete'
-      body_should_contain 'HTTP Basic: Access denied.'
+      pages.each do |page|
+        visit page
+        body_should_not_contain 'Kete'
+        body_should_contain 'HTTP Basic: Access denied.'
+      end
     end
 
   end
@@ -40,9 +50,11 @@ class SiteLockdownAuthTest < ActionController::IntegrationTest
     end
 
     should "not require that the viewer login" do
-      visit "/"
-      body_should_not_contain 'HTTP Basic: Access denied.'
-      body_should_contain 'Kete'
+      pages.each do |page|
+        visit page
+        body_should_not_contain 'HTTP Basic: Access denied.'
+        body_should_contain 'Kete'
+      end
     end
 
   end
