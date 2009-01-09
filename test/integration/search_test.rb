ab5f4bb3cc1e405de0f7c6e30af34b3d642888a3
Kieran Pilkington <kieran@katipo.co.nz>
2009-01-09 14:27:17 +1300

    test: Adding tests for titles, descriptions and tags which include special chars (ampersands, greater and lesser than chars). Also making sure quoted and unquoted searches work.

--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -58,6 +58,33 @@ class SearchTest < ActionController::IntegrationTest
 
     end
 
+    context "Topics with specials chars in title, description, and tags" do
+
+      setup do
+        @user = create_new_user({:login => login})
+        @user.add_as_member_to_default_baskets
+        @@users_created << @user
+        login_as(login)
+        @fields = { :title => 'One&Two', :description => 'Three<Four', :tag_list => 'Six>ive' }
+        @should_have = Regexp.new("<h4><a (.+)>One&amp;Two</a></h4>")
+        @topic = new_topic(@fields)
+      end
+
+      should "be able to be found when searching topics" do
+        make_search_at("/site/all/topics", @fields, @should_have)
+      end
+
+      should "be able to be found when searching contributors" do
+        make_search_at("/site/all/topics/contributed_by/user/#{@user.to_param}", @fields, @should_have)
+      end
+
+      should "be able to be found when searching taggings" do
+        tag = Tag.last
+        make_search_at("/site/all/topics/tagged/#{tag.id}", @fields, @should_have)
+      end
+
+    end
+
   end
 
   private
@@ -65,7 +92,12 @@ class SearchTest < ActionController::IntegrationTest
   def make_search_at(url, fields, should_have)
     visit url
     fields.each do |field,value|
-      fill_in 'search_terms', :with => value
+      # test unquoted
+      fill_in 'search_terms', :with => "#{value}"
+      click_button "Search"
+      body_should_contain should_have, :number_of_times => 1
+      # test quoted
+      fill_in 'search_terms', :with => "'#{value}'"
       click_button "Search"
       body_should_contain should_have, :number_of_times => 1
     end
