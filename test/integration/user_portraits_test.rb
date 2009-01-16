1017df6c4fc09e3f549857f4d0442410447fca6d
Walter McGinnis <walter@katipo.co.nz>
2009-01-16 22:36:44 +1300

    refinement: made a start on tests for user portraits

---
+++ test/integration/user_portraits_test.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + '/integration_test_helper'
+
+class AccountTest < ActionController::IntegrationTest
+  context "A user's profile" do
+    setup do
+      configure_environment do
+        set_constant :ENABLE_USER_PORTRAITS, true
+      end
+
+      add_joe_as_regular_user
+      login_as('joe')
+    end
+
+    should "not have selected portrait or other portraits if there are no portraits" do
+      visit '/'
+      click_link 'joe'
+      body_should_not_contain "selected portrait"
+      body_should_not_contain "Other Portraits"
+    end
+
+    context "for a user who has created one image" do
+      setup do
+        @item = new_still_image { attach_file "image_file_uploaded_data", "white.jpg" }
+      end
+
+      should "be able to add existing image to portraits and have it show up as selected image" do
+        item_base_url = "/site/images/show/#{@item.id}"
+        visit item_base_url
+        click_link 'Add image to portraits'
+        body_should_contain "'#{@item.title}' has been added to your portraits"
+        click_link 'joe'
+        body_should_contain 'Selected<br />Portrait'
+        body_should_contain Regexp.new("<a href=\"#{item_base_url}\"")
+      end
+    end
+
+    # TODO:
+    # test that shows that more than one portraits has "Other Portraits" on both image detail page and user account
+    # tests that handle non-javascript reordering
+    # ...
+  end
+
+end
