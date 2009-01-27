41deb42684016df7cbc1e3e4257ae1fdd3631a8e
Walter McGinnis <walter@katipo.co.nz>
2009-01-27 13:00:51 +1300

    small comment refinements and some additional logic refinement

--- lib/embedded.rb
+++ lib/embedded.rb
@@ -12,18 +12,18 @@ module Embedded
     # declare the virtual attribute that we can stuff the embedded metadata into
     attr_accessor :embedded
 
-    before_validation :harvest_embedded_metadata_to_attributes
+    before_validation :harvest_embedded_metadata_to_attributes unless self.class.is_a?(StillImage)
 
     # note, this isn't meant for StillImage
     # StillImage case where actually it is still_image's original ImageFile
     # we need to grab the data from and then pass up to the still_image object
-    # THIS HANDLES SIMPLE CASE, for audio, video, documents where they don't have a separate model
+    # THIS HANDLES SIMPLE CASE, for audio, video, documents where they DO NOT have a separate model
     # of attachments
     def harvest_embedded_metadata_to_attributes
       # read the metadata from the file and load it into embedded attribute
       self.embedded = MiniExiftool.new(self.full_filename)
 
-      # lastly we look at the mappings between
+      # look at the mappings between
       # either default fields (title, short_summary, tags, etc.)
       # or extended fields
 
@@ -61,9 +61,9 @@ module Embedded
 
               self.tag_list = all_tags.to_sentence
             else
-              # will overwrite any preview value that has been assigned to the attribute
-              # not append
-              self.send("#{a_name}=", value)
+              # will append any previous value for the field
+              # to preserve the value that may have been added in the form
+              self.send("#{a_name}+=", value)
             end
           end
           matching_extended_fields = ExtendedField.find(:all, :conditions => "import_synonyms like \'%#{key}%\'")
