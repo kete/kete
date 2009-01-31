9ca5d2613b1d4a6a641c16bcdaf46bc2049bf057
Walter McGinnis <walter@katipo.co.nz>
2009-01-31 13:45:14 +1300

    Putting in further explanation about how base url takes precedence when creating links on a choice value.

--- app/controllers/extended_fields_controller.rb
+++ app/controllers/extended_fields_controller.rb
@@ -1,7 +1,7 @@
 class ExtendedFieldsController < ApplicationController
-  
+
   helper ExtendedFieldsHelper
-  
+
   # everything else is handled by application.rb
   before_filter :login_required, :only => [:list, :index, :add_field_to_multiples]
 
@@ -13,10 +13,10 @@ class ExtendedFieldsController < ApplicationController
     # Default columns and column exclusions
     config.columns = [:label, :description, :xml_element_name, :ftype, :import_synonyms, :example, :multiple, :user_choice_addition]
     config.list.columns.exclude [:updated_at, :created_at, :topic_type_id, :xsi_type, :user_choice_addition]
-    
+
     config.columns << [:base_url]
     config.columns[:base_url].label = "Base URL"
-    config.columns[:base_url].description = "Whatever the user inputs will be appended to this base url (e.g. http://site.com/~[user_input])"
+    config.columns[:base_url].description = "Whatever the user inputs will be appended to this base url (e.g. http://site.com/~[user_input]).</p><p>&nbsp;</p><p><b>NOTE:</b> in the case of any of the choice ftypes, Base URL + value will take over from normal linking to all results that match the value for the choice for the extended field."
 
     # CRUD for adding/removing choices
     config.columns << [:pseudo_choices]
@@ -24,58 +24,58 @@ class ExtendedFieldsController < ApplicationController
     config.columns[:pseudo_choices].description = "Ftype must be a \"choices\" option for these options to be available to users."
     config.columns[:user_choice_addition].label = nil
   end
-  
+
   def add_field_to_multiples
-    
+
     extended_field = ExtendedField.find(params[:extended_field_id])
     n = params[:n].to_i
     @item_type_for_params = params[:item_key]
-    
+
     render :update do |page|
-     
+
       # Remove the field adder control
       page.remove "#{qualified_name_for_field(extended_field)}_multis_extender"
-      
+
       # Add a new field editor to the bottom of the set
       page.insert_html :bottom, "#{qualified_name_for_field(extended_field)}_multis", \
         :partial => 'extended_fields/extended_field_editor', \
         :locals => { :ef => extended_field, :content => [], :n => n, :multiple => true }
-        
+
       # Add the field adder control back to the bottom of the set
       page.insert_html :bottom, "#{qualified_name_for_field(extended_field)}_multis", \
         :partial => 'extended_fields/additional_extended_field_control', \
         :locals => { :ef => extended_field, :n => n.to_i + 1 }
     end
   end
-  
-  # Fetch subchoices for a choice. 
+
+  # Fetch subchoices for a choice.
   def fetch_subchoices
 
     extended_field = ExtendedField.find(params[:options][:extended_field_id])
-    
+
     # Find the current choice
     current_choice = params[:value].blank? ? \
       Choice.find_by_label(params[:label]) : Choice.find_by_value(params[:value])
-    
+
     choices = current_choice ? current_choice.children.select { |c| extended_field.choices.member?(c) } : []
-    
+
     options = {
       :choices => choices,
       :level => params[:for_level].to_i + 1,
       :extended_field => extended_field
     }
-    
-    # Ensure we have a standard environment to work with. Some parts of the helpers (esp. ID and NAME 
+
+    # Ensure we have a standard environment to work with. Some parts of the helpers (esp. ID and NAME
     # attribute generation rely on these.
     @item_type_for_params = params[:item_type_for_params]
     @field_multiple_id = params[:field_multiple_id]
-      
-    
+
+
     render :update do |page|
-      
+
       # Generate the DOM ID
       dom_id = "#{id_for_extended_field(options[:extended_field])}__level_#{params[:for_level]}"
-      
+
       if options[:choices].empty?
         page.replace_html dom_id, ""
       else
