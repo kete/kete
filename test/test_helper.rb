abbfa80d00a58cc07de56d65ba08b1697605a4fc
Kieran Pilkington <kieran@katipo.co.nz>
2009-02-16 18:50:34 +1300

    test: fixing the majority of failing tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -44,7 +44,9 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  include ActionController::TestProcess
 end
