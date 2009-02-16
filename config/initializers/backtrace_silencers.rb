0d8f0c4a84caa00bffdf8a2127c618ca3bd5225d
Kieran Pilkington <kieran@katipo.co.nz>
2009-02-16 22:40:00 +1300

    refinement: removing debug test output option, and adding shoulda to the list of things to clean from the backtrace. Errors have gone from 25 lines  before 2.3 to just 3. Yay!

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -2,6 +2,7 @@
 
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /shoulda/ }
 
 # You can also remove all the silencers if you're trying do debug a problem that might steem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
