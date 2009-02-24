16dc9e16af78c643715a4fbf5dc3cf9af6f3a5fa
Kieran Pilkington <kieran@katipo.co.nz>
2009-02-25 10:23:36 +1300

    bugfix: the error handling code no longer works with Rails 2.3 and will need to be adapted. Will investigate after translation work.

--- lib/error_handler.rb
+++ lib/error_handler.rb
@@ -1,62 +1,68 @@
-# Kieran Pilkington, 2008/07/28
-# We overwrite one of the Dispatch methods to give us the ability to make customized error pages
-# for errors that occur before controllers are called (like when memcache is not online)
-module ActionController
-  class Dispatcher
-    class << self
+#
+# Depreciation
+# The following code no longer works on Rails 2.3 and needs to be adapted for the new code
+# See ActionController::Dispatcher and ActionController::Failsafe
+#
 
-      def failsafe_response(fallback_output, status, originating_exception = nil)
-        yield
-      rescue Exception => exception
-        begin
-          log_failsafe_exception(status, originating_exception || exception)
-          #failsafe_logger.info(exception)
-          case exception.to_s
-            when 'No connection to server' then
-              body = failsafe_memcached_offline_body(status)
-              body = body.gsub(/MEMCACHED_ERROR/, exception.to_s)
-            else
-              body = failsafe_response_body(status)
-          end
-          fallback_output.write body
-          nil
-        rescue Exception => failsafe_error # Logger or IO errors
-          $stderr.puts "Error during failsafe response: #{failsafe_error}"
-          $stderr.puts "(originally #{originating_exception})" if originating_exception
-        end
-      end
-
-      private
-
-        def failsafe_response_body(status)
-          error_path = "#{error_file_path}/#{status.to_s[0...3]}.html"
-
-          if File.exist?(error_path)
-            File.read(error_path)
-          else
-            "<html>
-            <body>
-            <h1>#{status}</h1>
-            </body>
-            </html>"
-          end
-        end
-
-        def failsafe_memcached_offline_body(status)
-          error_path = "#{error_file_path}/memcached_offline.html"
-          if File.exist?(error_path)
-            File.read(error_path)
-          else
-            "<html>
-            <body>
-            <h1>#{status}</h1>
-            <h2>Memcached Error</h2>
-            <h3>MEMCACHED_ERROR</h3>
-            </body>
-            </html>"
-          end
-        end
-
-    end
-  end
-end
+# # Kieran Pilkington, 2008/07/28
+# # We overwrite one of the Dispatch methods to give us the ability to make customized error pages
+# # for errors that occur before controllers are called (like when memcache is not online)
+# module ActionController
+#   class Dispatcher
+#     class << self
+# 
+#       def failsafe_response(fallback_output, status, originating_exception = nil)
+#         yield
+#       rescue Exception => exception
+#         begin
+#           log_failsafe_exception(status, originating_exception || exception)
+#           #failsafe_logger.info(exception)
+#           case exception.to_s
+#             when 'No connection to server' then
+#               body = failsafe_memcached_offline_body(status)
+#               body = body.gsub(/MEMCACHED_ERROR/, exception.to_s)
+#             else
+#               body = failsafe_response_body(status)
+#           end
+#           fallback_output.write body
+#           nil
+#         rescue Exception => failsafe_error # Logger or IO errors
+#           $stderr.puts "Error during failsafe response: #{failsafe_error}"
+#           $stderr.puts "(originally #{originating_exception})" if originating_exception
+#         end
+#       end
+# 
+#       private
+# 
+#         def failsafe_response_body(status)
+#           error_path = "#{error_file_path}/#{status.to_s[0...3]}.html"
+# 
+#           if File.exist?(error_path)
+#             File.read(error_path)
+#           else
+#             "<html>
+#             <body>
+#             <h1>#{status}</h1>
+#             </body>
+#             </html>"
+#           end
+#         end
+# 
+#         def failsafe_memcached_offline_body(status)
+#           error_path = "#{error_file_path}/memcached_offline.html"
+#           if File.exist?(error_path)
+#             File.read(error_path)
+#           else
+#             "<html>
+#             <body>
+#             <h1>#{status}</h1>
+#             <h2>Memcached Error</h2>
+#             <h3>MEMCACHED_ERROR</h3>
+#             </body>
+#             </html>"
+#           end
+#         end
+# 
+#     end
+#   end
+# end
