c2c84f352732c95eca4c7c2ad9648663c9c79a37
Kieran Pilkington <kieran@katipo.co.nz>
2009-03-09 12:19:57 +1300

    bugfix: double // was causing routing errors.

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -113,7 +113,7 @@ module AuthenticatedSystem
     # to the passed default.
     def redirect_back_or_default(default, lang=I18n.locale)
       locale_match = %r(^/(#{I18n.available_locales.map{|l|l.to_s}.join('|')})(?=/|$))
-      session[:return_to] ? redirect_to(session[:return_to].gsub(locale_match, "/#{lang}/")) : redirect_to(default)
+      session[:return_to] ? redirect_to(session[:return_to].gsub(locale_match, "/#{lang}")) : redirect_to(default)
       session[:return_to] = nil
     end
 
