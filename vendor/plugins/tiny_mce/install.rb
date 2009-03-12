f2be77bc07a47f8221c7d5518d8587db0b7f72da
Kieran Pilkington <kieran@katipo.co.nz>
2009-03-12 17:57:48 +1300

    Reimporting tiny_mce plugin

---
+++ vendor/plugins/tiny_mce/install.rb
@@ -0,0 +1,8 @@
+# Lets output something to the console when people use script/plugin to install tiny_mce
+puts "-------"
+puts "tiny_mce plugin"
+puts ""
+puts "Thanks for installing the tiny_mce plugin."
+puts "To complete the process, please view README.rdoc."
+puts "Read the section entitled 'Installation"
+puts "-------"
