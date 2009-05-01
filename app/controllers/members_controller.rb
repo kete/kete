1b955cff388e099f39c1644705e95d3f1fca3a83
Kieran Pilkington <kieran@katipo.co.nz>
2009-05-01 12:01:12 +1200

    bugfix: memberlist was failin because it uses the same action menu as baskets but didn't have the helpers for it

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,6 +6,8 @@ class MembersController < ApplicationController
 
   before_filter :permitted_to_remove_basket_members, :only => [:remove]
 
+  helper :baskets
+
   def index
     redirect_to :action => 'list'
   end
