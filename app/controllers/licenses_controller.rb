aa2b92f63055f983f44728818b661426ce4b4d51
Kieran Pilkington <kieran@katipo.co.nz>
2009-05-08 12:27:05 +1200

    enhancement: you can now run license import rake tasks from within the licenses controller.

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,3 +1,8 @@
+require 'rake'
+require 'rake/rdoctask'
+require 'rake/testtask'
+require 'tasks/rails'
+
 class LicensesController < ApplicationController
 
   before_filter :login_required
@@ -7,6 +12,26 @@ class LicensesController < ApplicationController
   active_scaffold :license do |config|
     config.columns = [:name, :description, :url, :image_url, :metadata, :is_available, :is_creative_commons]
     list.columns.exclude [:updated_at, :created_at, :metadata, :description, :image_url, :is_available, :is_creative_commons, :users]
+  end
+
+  def install_license
+    if params[:task]
+      if LICENSE_TASKS.key?(params[:task])
+        old_license_count = License.count
+        ENV['RAILS_ENV'] = RAILS_ENV
+        rake_result = Rake::Task["acts_as_licensed:#{params[:task]}"].execute(ENV)
+        if rake_result || License.count > old_license_count
+          flash[:notice] = "Successfully imported licenses."
+        else
+          flash[:error] = "There was a problem importing the licenses."
+        end
+      else
+        flash[:error] = "Invalid license type."
+      end
+    else
+      flash[:error] = "No licenses to import were selected."
+    end
+    redirect_to :urlified_name => 'site', :controller => 'licenses', :action => 'list'
   end
 
   private
---
+++ config/initializers/licenses.rb
@@ -0,0 +1,4 @@
+LICENSE_TASKS = {
+  'import_nz_cc_licenses' => 'New Zealand Creative Commons Licenses',
+  'import_au_cc_licenses' => 'Australian Creative Commons Licenses'
+}
