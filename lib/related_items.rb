dfe0fa3afe1d4c6904c2b48308e7a251c8a2c52a
Walter McGinnis <walter@katipo.co.nz>
2009-05-11 13:01:57 +1200

    taking out no longer accurate comment

--- lib/related_items.rb
+++ lib/related_items.rb
@@ -3,14 +3,6 @@ module RelatedItems
   unless included_modules.include? RelatedItems
     # replaces oai_xml_helpers#related_items_of
     def related_items_hash
-      # in the case we are adding relations or bulk importing, and multiple OAI records are
-      # generated in the same controller request, we need to have a unique memoized instance
-      # var for each item type and id. Having one single one (i.e. @related_items ||= ) won't
-      # work because a collision occurs and results aren't regenerated, resulting in the child
-      # item having the same memoized related items as the parent and the related items
-      # functionality stops working correctly. Using instance_eval allows us to fix this issue
-      # by generation a unique instance var that can be used by the same OAI generation later
-      # but not by any other items OAI generation
       @related_items_hash ||= begin
         related_items_hash = Hash.new
         related_items_hash['Topic'] = self.is_a?(Topic) ? related_topics : topics
