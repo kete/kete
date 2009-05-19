8e35ff69acd3a2f58ecb8f2b399cda4074af4818
Kieran Pilkington <kieran@katipo.co.nz>
2009-05-19 16:10:05 +1200

    refinement: cleanup of the basket_urlified_name code.

--- lib/configure_acts_as_zoom_for_kete.rb
+++ lib/configure_acts_as_zoom_for_kete.rb
@@ -9,23 +9,18 @@ module ConfigureActsAsZoomForKete
   # aren't available
   unless included_modules.include? ConfigureActsAsZoomForKete
     attr_accessor :oai_record
-    attr_accessor :basket_urlified_name
     def self.included(klass)
-      # important that this goes before acts_as_zoom declaration
-      klass.send :before_destroy, :prepare_for_zoom_id
-      klass.send :private, :prepare_for_zoom_id
-
       klass.send :acts_as_zoom, :fields => [:oai_record],
                                 :save_to_public_zoom => ['localhost', 'public'],
                                 :save_to_private_zoom => ['localhost', 'private'],
                                 :raw => true,
-                                :additional_zoom_id_attribute => :prepare_for_zoom_id,
+                                :additional_zoom_id_attribute => :basket_urlified_name,
                                 :use_save_callback => false
 
     end
 
-    def prepare_for_zoom_id
-      self.basket_urlified_name = self.basket.urlified_name
+    def basket_urlified_name
+      @basket_urlified_name = self.basket.urlified_name
     end
   end
 end
