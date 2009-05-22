f79d11e5bcd3bc8c29bfd059075fb911a0411d5f
Walter McGinnis <walter@katipo.co.nz>
2009-05-22 13:21:45 +1200

    bugfix: extending our tests to make sure question based captcha works.

--- test/integration/register_test.rb
+++ test/integration/register_test.rb
@@ -24,7 +24,53 @@ class RegisterTest < ActionController::IntegrationTest
       body_should_contain '1 error prohibited this user from being saved'
       body_should_contain 'Security code : Your security question answer failed - please try again.'
     end
-
   end
 
+  context "When a user views signup form, the user" do
+
+    should "get an option to use text question if site is configured to provide all captchas (the default)" do
+      # all is the default for captcha type setting
+      assert_equal CAPTCHA_TYPE, 'all'
+      visit '/site/account/signup'
+      body_should_contain 'Security Code: Please enter the text from the image to the right'
+      body_should_contain 'non-image security question'
+    end
+
+    should "if they have chosen question captcha, get question version of captcha" do
+      visit '/site/account/signup?captcha_type=question'
+      body_should_not_contain 'Security Code: Please enter the text from the image to the right'
+      body_should_not_contain 'non-image security question'
+      body_should_contain "brain_buster_captcha"
+    end
+
+    should "get text question if site is configured to only provide question captcha" do
+      configure_environment do
+        set_constant :CAPTCHA_TYPE, 'question'
+      end
+
+      visit '/site/account/signup'
+
+      body_should_not_contain 'non-image security question'
+      body_should_not_contain 'Security Code: Please enter the text from the image to the right'
+      body_should_contain "brain_buster_captcha"
+    end
+
+    should "get an image captcha if site is configured to only provide image captcha" do
+      configure_environment do
+        set_constant :CAPTCHA_TYPE, 'image'
+      end
+
+      visit '/site/account/signup'
+
+      body_should_not_contain 'non-image security question'
+      body_should_contain 'Security Code: Please enter the text from the image to the right'
+    end
+
+    teardown do
+      configure_environment do
+        set_constant :CAPTCHA_TYPE, 'all'
+      end
+    end
+
+  end
 end
