b5e94b6bdcec7a4fd8f1949a399d2c0b2df2e2a5
Kieran Pilkington <kieran@katipo.co.nz>
2009-05-29 16:26:17 +1200

    enhancement: allow fields without items that are using it to have their mappings deleted. Extend the same code from topic types to content types, and because of the duplication, took some time to refactor code (only controllers, the views are still duplicated and could do with cleaning up)

--- app/controllers/content_types_controller.rb
+++ app/controllers/content_types_controller.rb
@@ -1,16 +1,5 @@
 class ContentTypesController < ApplicationController
-  # everything else is handled by application.rb
-  before_filter :login_required, :only => [:list, :index]
-
-  permit "site_admin or admin of :site"
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def index
-    redirect_to :action => 'list'
-  end
+  include FieldMappingsController
 
   def list
     @content_types = ContentType.paginate(:page => params[:page], :per_page => 10, :order => 'controller')
@@ -20,73 +9,8 @@ class ContentTypesController < ApplicationController
     @content_type = ContentType.new
   end
 
-  def create
-    @content_type = ContentType.new(params[:content_type])
-    if @content_type.save
-      # TODO: globalize translate
-      flash[:notice] = 'Content type was successfully created.'
-      redirect_to :action => 'edit', :id => @content_type
-    else
-      render :action => 'new'
-    end
-  end
-
   def edit
     @content_type = ContentType.find(params[:id])
-  end
-
-  def update
-    @content_type = ContentType.find(params[:id])
-    if @content_type.update_attributes(params[:content_type])
-      # TODO: globalize translate
-      flash[:notice] = 'Content type was successfully updated.'
-      redirect_to :action => 'edit', :id => @content_type
-    else
-      render :action => 'edit'
-    end
-  end
-
-  # TODO: possibly remove, at the least put high restrictions on
-  def destroy
-    ContentType.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
-
-  def add_to_content_type
-    content_type = ContentType.find(params[:id])
-
-    # this is setup for a form that has multiple fields
-    # we want to separate out plain form fields from required ones
-
-    # params has a hash of hashes for field with field_id as the key
-    params[:extended_field].keys.each do |field_id|
-      field = ExtendedField.find(field_id)
-
-      # into the field's hash
-      # now we can grab the field's attributes that are being updated
-      params[:extended_field][field_id].keys.each do |to_add_attr|
-        to_add_attr_value = params[:extended_field][field_id][to_add_attr]
-
-        # if we are supposed to add the field
-        if to_add_attr_value.to_i == 1
-
-          # determine if it should be a required field
-          # or just an optional one
-          if to_add_attr =~ /required/
-            content_type.required_form_fields << field
-          else
-            content_type.form_fields << field
-          end
-        end
-      end
-    end
-    redirect_to :action => :edit, :id => content_type
-  end
-
-  def reorder_fields_for_content_type
-    # update position in the content_type's form
-    ContentTypeToFieldMapping.update(params[:mapping].keys, params[:mapping].values)
-    redirect_to :action => :edit, :id => params[:id]
   end
 
 end
--- app/controllers/topic_types_controller.rb
+++ app/controllers/topic_types_controller.rb
@@ -1,114 +1,20 @@
 class TopicTypesController < ApplicationController
-  # everything else is handled by application.rb
-  before_filter :login_required, :only => [:list, :index]
-
-  permit "site_admin or admin of :site"
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def index
-    list
-    render :action => 'list'
-  end
+  include FieldMappingsController
 
   def list
     @topic_types = TopicType.find(1).full_set.paginate(:page => params[:page], :per_page => 10)
-  end
-
-  def show
-    @topic_type = TopicType.find(params[:id])
   end
 
   def new
     @topic_type = TopicType.new
   end
 
-  def create
-    @topic_type = TopicType.new(params[:topic_type])
-    if @topic_type.save
-      set_ancestory(@topic_type)
-
-      # TODO: globalize translate
-      flash[:notice] = 'TopicType was successfully created.'
-      redirect_to :urlified_name => @site_basket.urlified_name, :action => 'edit', :id => @topic_type
-    else
-      render :action => 'new'
-    end
-  end
-
   def edit
     @topic_type = TopicType.find(params[:id])
   end
 
-  def update
-    @topic_type = TopicType.find(params[:id])
-    if @topic_type.update_attributes(params[:topic_type])
-      # this isn't a move, so resetting ancestory isn't necessary
-      # set_ancestory(@topic_type)
-
-      # expire show details for all topics of this type
-      # since it displays the topic_type.name
-      @topic_type.topics.each do |topic|
-        expire_fragment(:controller => 'topics', :urlified_name => topic.basket.urlified_name, :action => 'show', :id => topic, :part => 'details')
-      end
-
-      # TODO: globalize translate
-      flash[:notice] = 'TopicType was successfully updated.'
-      redirect_to :urlified_name => @site_basket.urlified_name, :action => 'edit', :id => @topic_type
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @topic_type = TopicType.find(params[:id])
-    @successful = @topic_type.destroy
-    if @successful
-      flash[:notice] = 'TopicType was successfully deleted.'
-      redirect_to :urlified_name => @site_basket.urlified_name, :action => 'list'
-    end
-  end
-
-  def add_to_topic_type
-    topic_type = TopicType.find(params[:id])
-
-    # this is setup for a form that has multiple fields
-    # we want to separate out plain form fields from required ones
-
-    # params has a hash of hashes for field with field_id as the key
-    params[:extended_field].keys.each do |field_id|
-      field = ExtendedField.find(field_id)
-
-      # into the field's hash
-      # now we can grab the field's attributes that are being updated
-      params[:extended_field][field_id].keys.each do |to_add_attr|
-        to_add_attr_value = params[:extended_field][field_id][to_add_attr]
-
-        # if we are supposed to add the field
-        if to_add_attr_value.to_i == 1
-
-          # determine if it should be a required field
-          # or just an optional one
-          if to_add_attr =~ /required/
-            topic_type.required_form_fields << field
-          else
-            topic_type.form_fields << field
-          end
-        end
-      end
-    end
-    redirect_to :urlified_name => @site_basket.urlified_name, :action => 'edit', :id => topic_type
-  end
-
-  def reorder_fields_for_topic_type
-    # update position in the topic_type's form
-    TopicTypeToFieldMapping.update(params[:mapping].keys, params[:mapping].values)
-    redirect_to :urlified_name => @site_basket.urlified_name, :action => 'edit', :id => params[:id]
-  end
-
   private
+
   def set_ancestory(topic_type)
       # setup of ancestory
       parent_id = params[:parent_id] || topic_type.parent_id || 1
