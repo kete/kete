9ac625aa9f45d40efaad78317a878a0ec5f8e682
Kieran Pilkington <kieran@katipo.co.nz>
2009-07-09 12:34:02 +1200

    Updating external search source engine

--- vendor/plugins/external_search_sources/init.rb
+++ vendor/plugins/external_search_sources/init.rb
@@ -1,5 +1,5 @@
-['en'].each do |locale|
-  I18n.load_path.unshift(File.dirname(__FILE__) + "/config/locales/#{locale}.yml")
+Dir[File.join("#{File.dirname(__FILE__)}/config/locales/*.yml")].each do |locale|
+  I18n.load_path.unshift(locale)
 end
 
 ActionController::Base.send(:helper, SearchSourcesHelper)
