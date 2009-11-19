5f14b4684d7da1391d51ce094c107b6df16e1170
Kieran Pilkington <kieran@katipo.co.nz>
2009-11-19 16:34:25 +1300

    bugfix: the links for topic type search were pointing to the wrong place.

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -4,7 +4,8 @@ module TopicsHelper
     topic_types_and_counts = topic.related_items.collection_of_objects_and_counts_for(:topic_type, true)
     topic_types_and_counts.each do |topic_type, count|
       title = "#{h(topic_type.name.pluralize)} (#{count})"
-      html += "<li>" + link_to(title, url_for_topics_of_type(topic_type)) + "</li>"
+      html += "<li>" + link_to_related_items_of(topic, 'Topic', { :link_text => title },
+                                                { :topic_type => topic_type.urlified_name }) + "</li>"
     end
     html += "</ul>"
     html
