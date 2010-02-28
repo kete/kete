0a4c2dad83a53dc24d8eb3e30f231ea9f661818f
Kieran Pilkington <kieran@katipo.co.nz>
2010-03-01 11:27:02 +1300

    refinement: Refining a search was losing some data, like which contributor you were viewing, or which item type. Some styling fixes too.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -81,6 +81,30 @@ module SearchHelper
     statement.join(', ') + " [ " + links.join(' | ') + " ] "
   end
 
+  # Clear any values that shouldn't be there when we make a new search or refine a search
+  def overwritten_search_params
+    {
+      # set basic values for our search
+      :urlified_name => @current_basket.urlified_name,
+      :controller => 'search',
+      :action => 'terms_to_page_url_redirect',
+
+      # clear these from the params so the form fields take effect instead
+      :search_terms => nil,
+      :date_since => nil,
+      :date_until => nil,
+      :topic_type => nil,
+      :privacy_type => nil,
+      :sort_type => nil,
+      :sort_direction => nil,
+      :limit_to_choice => nil,
+      :extended_field => nil,
+
+      # no sense in keeping page number, new results could be much less causing 404's
+      :page => nil
+    }
+  end
+
   # Used to check if an item is part of an existing relationship in related items search
   def related?(item)
     !@existing_ids.nil? && @existing_ids.member?(item.id)
