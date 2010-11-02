05b5910525c7e8853f4e75206fa3ce7bd5d7726f
Walter McGinnis <walter@katipo.co.nz>
2010-11-02 16:27:07 +1300

    enhancment: #296 - topic type ancestry breadcrumb

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,33 @@
 module TopicsHelper
+  def topic_type_breadcrumb_for(topic)
+    html = "<ul class=\"breadcrumb\">"
+    count = 0
+    topic_types = topic.topic_type.self_and_ancestors
+    topic_types.each do |tt|
+      count += 1
+      html += "<li class=\""
+
+      classes = Array.new
+      classes << "first" if count == 1
+
+      if topic_types.size == count
+        classes << "selected-topic-type"
+      else
+        classes << "ancestor-topic-type"
+      end
+
+      html += classes.join(" ") +"\">"
+
+      unless count == 1
+        html += "<span class=\"breadcrumb-delimiter\">"
+        html += t('base.breadcrumb_delimiter')
+        html += "</span>"
+      end
+      html += link_to(h(tt.name), url_for_topics_of_type(tt)) + "</li>"
+    end
+    html += "</ul>"
+  end
 end
+
+# add first class to first item
+# add ancestor class to any item besides last
