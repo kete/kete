326598b86975b5ab5a4a68f7c311a51740a42175
Walter McGinnis <walter@katipo.co.nz>
2010-11-15 14:45:38 +1300

    Updated some names and replaced some failing hardcoded link names.

--- test/integration/moderation_test.rb
+++ test/integration/moderation_test.rb
@@ -86,7 +86,7 @@ class ModerationTest < ActionController::IntegrationTest
 
         visit "/site/topic_types/edit/#{@topic_type.id.to_s}"
 
-        check "extended_field_#{@extended_field.id.to_s}_required_checkbox"
+        check "extended_field_#{@extended_field.to_param.to_s}_required_checkbox"
         click_button "Add to Topic Type"
         
         update_item @topic do
@@ -115,7 +115,7 @@ class ModerationTest < ActionController::IntegrationTest
       
       should "force content update when reverting to the first version" do
         visit "/#{@basket.urlified_name}/topics/preview/#{@topic.id}?version=1"
-        click_link "Make this revision live"
+        click_link I18n.t('topics.preview_actions.make_live')
         body_should_contain "The version you're reverting to is missing some compulsory content. Please contribute the missing details before continuing. You may need to contact the original author to collect additional information."
         
         fill_in "topic[extended_content_values][required_field]", :with => "Value for required field after moderation"
@@ -130,7 +130,7 @@ class ModerationTest < ActionController::IntegrationTest
         end
         
         visit "/#{@basket.urlified_name}/topics/preview/#{@topic.id}?version=2"
-        click_link "Make this revision live"
+        click_link I18n.t('topics.preview_actions.make_live')
         
         body_should_contain "The content of this Topic has been approved from the selected revision."
         body_should_contain "Value for required field"
@@ -145,7 +145,7 @@ class ModerationTest < ActionController::IntegrationTest
         
         should "revert to first version, supply additional content and have version made live immediately" do
           visit "/#{@basket.urlified_name}/topics/preview/#{@topic.id}?version=1"
-          click_link "Make this revision live"
+          click_link I18n.t('topics.preview_actions.make_live')
           
           body_should_contain "The version you're reverting to is missing some compulsory content. Please contribute the missing details before continuing. You may need to contact the original author to collect additional information."
           
@@ -161,7 +161,7 @@ class ModerationTest < ActionController::IntegrationTest
 
         should "be able to delete all versions of item from preview of version" do
           visit "/#{@basket.urlified_name}/topics/preview/#{@topic.id}?version=1"
-          click_link "delete this item completely"
+          click_link I18n.t('topics.preview_actions.delete_all_versions')
           assert_equal "http://www.example.com/en/moderation_test_basket/all/topics/", current_url
         end
         
@@ -254,7 +254,7 @@ class ModerationTest < ActionController::IntegrationTest
       click_button "Create"
 
       verb = options[:extended_field_value_required] ? "required" : "add"
-      check "extended_field_#{ExtendedField.last.id.to_s}_#{verb}_checkbox"
+      check "extended_field_#{ExtendedField.last.to_param.to_s}_#{verb}_checkbox"
       click_button "Add to Topic Type"
 
       text_verb = options[:extended_field_value_required] ? "required" : "optional"
