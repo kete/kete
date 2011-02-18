fbebdf5eace52d469aa7f9916e5bfb7a377ab454
Walter McGinnis <walter@katipo.co.nz>
2011-02-19 12:07:31 +1300

    Added logic change to handle Kete.site_name responding, but returning nil.

--- config/initializers/oembed_provider.rb
+++ config/initializers/oembed_provider.rb
@@ -5,7 +5,7 @@ if Rails.env == 'test'
   OembedProvider.provider_url = "http://example.com"
 else
   OembedProvider.provider_name = Kete.respond_to?(:pretty_site_name) ? Kete.pretty_site_name : "Kete waiting configuration"
-  OembedProvider.provider_url = Kete.respond_to?(:site_name) ? Kete.site_name : "http://placeholder_url"
+  OembedProvider.provider_url = Kete.respond_to?(:site_name) && Kete.site_name.present? ? Kete.site_name : "http://placeholder_url"
 end
 
 OembedProvider.controller_model_maps = { 'images' => 'StillImage' }
