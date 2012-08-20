74684e363ac60556de2fba1ac07e426f33c27c1c
Walter McGinnis <wm@waltermcginnis.com>
2012-08-20 19:18:06 +1200

    Adding ability for scope helpers to handle exact pattern matching rather than right hand globbing.

--- lib/extended_content_scope_helpers.rb
+++ lib/extended_content_scope_helpers.rb
@@ -1,14 +1,16 @@
 # class methods useful for defining scopes specific to an extended field
 # requires that class being extended has been extended with ExtendedContentHelpers
 module ExtendedContentScopeHelpers
-  def field_condition_sql_for(pattern, field_id)
+  def field_condition_sql_for(pattern, field_id, exact_match = false)
     field = ExtendedField.find(field_id)
     xml = Nokogiri::XML::Builder.new
-    
+
+    pattern = "#{pattern}%" unless exact_match
+
     extended_content_field_xml_tag(
                                    :xml => xml,
                                    :field => field.label_for_params,
-                                   :value => "#{pattern}%",
+                                   :value => pattern,
                                    :xml_element_name => field.xml_element_name,
                                    :xsi_type => field.xsi_type,
                                    :extended_field => field
