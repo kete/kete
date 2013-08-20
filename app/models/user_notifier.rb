76e582a012451b8471090fcab079a894ab028f6b
Rob Ramsay <robert.ramsay.nz@gmail.com>
2013-08-21 02:02:29 +1200

    rails:upgrade:check: Old ActionMailer class API http://lindsaar.net/2010/1/26/new-actionmailer-api-in-rails-3

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -1,4 +1,6 @@
 class UserNotifier < ActionMailer::Base
+  default :from => "#{Kete.notifier_email}"
+
   # kludge for A_S and rails 2.0
   def generic_view_paths
     []
@@ -7,74 +9,90 @@ class UserNotifier < ActionMailer::Base
   def forgot_password(user)
     setup_email(user)
     @subject    += I18n.t('user_notifier_model.password_change')
-    @body[:url]  = "#{Kete.site_url}site/account/reset_password/#{ user.password_reset_code}"
+    @url  = "#{Kete.site_url}site/account/reset_password/#{ user.password_reset_code}"
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def reset_password(user)
     setup_email(user)
     @subject    += I18n.t('user_notifier_model.password_reset')
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def signup_notification(user)
     setup_email(user)
     @subject    += I18n.t('user_notifier_model.activate_account')
-    @body[:url]  = "#{Kete.site_url}site/account/activate/#{user.activation_code}"
+    @url  = "#{Kete.site_url}site/account/activate/#{user.activation_code}"
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def notification_to_administrators_of_new(user, admin)
     setup_email(admin)
     @subject    += I18n.t('user_notifier_model.review_new_account', :new_user => user.resolved_name)
-    @body[:new_user] = user
-    @body[:url]  = "#{Kete.site_url}site/account/show/#{user.id}"
+    @new_user = user
+    @url  = "#{Kete.site_url}site/account/show/#{user.id}"
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def activation(user)
     setup_email(user)
     @subject    += I18n.t('user_notifier_model.account_activated')
-    @body[:url]  = "#{Kete.site_url}"
+    @url  = "#{Kete.site_url}"
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def banned(user)
     setup_email(user)
     @subject    += I18n.t('user_notifier_model.account_banned')
-    @body[:url]  = "#{Kete.site_url}"
+    @url  = "#{Kete.site_url}"
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def email_to(recipient, sender, subject, message, from_basket = nil)
     setup_email(sender)
     @recipients = recipient.email
     @reply_to = sender.email
-    @subject += I18n.t('user_notifier_model.user_sent_message', :user_name => sender.user_name)
-    @body[:recipient] = recipient
-    @body[:subject] = subject
-    @body[:message] = message
-    @body[:from_basket] = from_basket
+    @subject += I18nt('user_notifier_model.user_sent_message', :user_name => sender.user_name)
+    @recipient = recipient
+    @subject = subject
+    @message = message
+    @from_basket = from_basket
+
+    mail(:to => @recipients, :subject => @subject, :reply_to => @reply_to)
   end
 
   # notifications for basket joins
   def join_notification_to(recipient, sender, basket, type)
     setup_email(recipient)
-    @body[:sender] = sender
-    @body[:basket] = basket
+    @sender = sender
+    @basket = basket
 
     case type
     when 'joined'
       @subject += I18n.t('user_notifier_model.user_joined',
                          :user_name => sender.user_name, :basket_name => basket.name)
-      @template = 'user_notifier/join_policy/member'
+      @template_name = 'join_policy/member'
     when 'request'
       @subject += I18n.t('user_notifier_model.user_requested',
                          :user_name => sender.user_name, :basket_name => basket.name)
-      @template = 'user_notifier/join_policy/request'
+      @template_name = 'join_policy/request'
     when 'approved'
       @subject += I18n.t('user_notifier_model.membership_accepted', :basket_name => basket.name)
-      @template = 'user_notifier/join_policy/accepted'
+      @template_name = 'join_policy/accepted'
     when 'rejected'
       @subject += I18n.t('user_notifier_model.membership_rejected', :basket_name => basket.name)
-      @template = 'user_notifier/join_policy/rejected'
+      @template_name = 'join_policy/rejected'
     else
       raise "Invalid membership notification type. joined, request, approved and rejected only."
     end
+
+    mail(:to => @recipients, :subject => @subject, :template_name => @template_name)
   end
 
   # notifications for flagging/moderation
@@ -82,67 +100,81 @@ class UserNotifier < ActionMailer::Base
     setup_email(moderator)
     @subject += I18n.t('user_notifier_model.item_flagged_with', :flag => flag)
     setup_body_with(revision, url, message, submitter)
-    @body[:flagging_user]  = flagging_user
-    @body[:flag] = flag
+    @flagging_user  = flagging_user
+    @flag = flag
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def pending_review_for(revision, submitter)
     setup_email(submitter)
     @subject += I18n.t('user_notifier_model.pending_moderation', :flag => Kete.pending_flag)
-    @body[:revision] = revision
+    @revision = revision
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def review_flagged_for(basket, moderator)
     setup_email(moderator)
     @subject += I18n.t('user_notifier_model.awaiting_review', :basket_name => basket.name)
-    @body[:basket] = basket
-    @body[:disputed_revisions] = basket.all_disputed_revisions
+    @basket = basket
+    @disputed_revisions = basket.all_disputed_revisions
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def rejection_of(revision, url, submitter, rejection_message)
     setup_email(submitter)
     @subject += I18n.t('user_notifier_model.rejected_submission', :flag => Kete.rejected_flag)
     setup_body_with(revision, url, rejection_message)
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def approval_of(revision, url, submitter, approval_message)
     setup_email(submitter)
     @subject    += I18n.t('user_notifier_model.revision_live')
     setup_body_with(revision, url, approval_message)
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   def reviewing_of(revision, url, submitter, rejection_message)
     setup_email(submitter)
     @subject += I18n.t('user_notifier_model.reviewed_submission', :flag => Kete.reviewed_flag)
     setup_body_with(revision, url, rejection_message)
+
+    mail(:to => @recipients, :subject => @subject)
   end
 
   # notications for baskets
   def basket_notification_to(recipient, sender, basket, type)
     setup_email(recipient)
-    @body[:sender] = sender
-    @body[:basket] = basket
+    @sender = sender
+    @basket = basket
 
     case type
     when 'created'
       @subject += I18n.t('user_notifier_model.basket_created',
                          :user_name => sender.user_name, :basket_name => basket.name)
-      @body[:needs_approval] = false
-      @template = 'user_notifier/basket_create_policy/created'
+      @needs_approval = false
+      @template_name = 'basket_create_policy/created'
     when 'request'
       @subject += I18n.t('user_notifier_model.basket_requested',
                          :user_name => sender.user_name, :basket_name => basket.name)
-      @body[:needs_approval] = true
-      @template = 'user_notifier/basket_create_policy/created'
+      @needs_approval = true
+      @template_name = 'basket_create_policy/created'
     when 'approved'
       @subject += I18n.t('user_notifier_model.basket_approved', :basket_name => basket.name)
-      @template = 'user_notifier/basket_create_policy/approved'
+      @template_name = 'basket_create_policy/approved'
     when 'rejected'
       @subject += I18n.t('user_notifier_model.basket_rejected', :basket_name => basket.name)
-      @template = 'user_notifier/basket_create_policy/rejected'
+      @template_name = 'basket_create_policy/rejected'
     else
       raise "Invalid basket notification type. created, request, approved and rejected only."
     end
+
+    mail(:to => @recipients, :subject => @subject, :template_name => @template_name)
   end
 
   def login_changed(user)
@@ -163,41 +195,43 @@ class UserNotifier < ActionMailer::Base
 
       if basket.settings[:private_item_notification_show_title] == true
         @subject += I18n.t("user_notifier_model.private_#{type}_with_title", :basket_name => basket.name, :item_title => item.title)
-        @body[:title] = item.title
+        @title = item.title
       else
         @subject += I18n.t("user_notifier_model.private_#{type}", :basket_name => basket.name)
-        @body[:title] = nil
+        @title = nil
       end
 
       if item.respond_to?(:short_summary) && basket.settings[:private_item_notification_show_short_summary] == true
-        @body[:summary] = item.short_summary
+        @summary = item.short_summary
       else
-        @body[:summary] = nil
+        @summary = nil
       end
 
-      @body[:item] = item
-      @body[:url] = url
-      @body[:type] = type
-      @template = "user_notifier/private_item_notification"
+      @item = item
+      @url = url
+      @type = type
+      @template_name = "private_item_notification"
+
+      mail(:to => @recipients, :subject => @subject, :template_name => @template_name)
     end
   end
 
   protected
 
-  def setup_email(user)
+  def setup_email(user)     
     @recipients  = "#{user.email}"
-    @from        = "#{Kete.notifier_email}"
     @subject     = "#{Kete.site_name} "
-    @sent_on     = Time.now
-    @body[:user] = user
-    @body[:recipient] = user # less confusing than user
+
+    @user = user
+    @recipient = user # less confusing than user
   end
 
+
   def setup_body_with(revision, url, message, submitter = nil)
-    @body[:revision] = revision
-    @body[:url] = url
-    @body[:submitter] = submitter
-    @body[:message] = message
+    @revision = revision
+    @url = url
+    @submitter = submitter
+    @message = message
   end
 
   # James - 2008-06-29
