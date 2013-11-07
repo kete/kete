d577e4f684c6214ff9ef2e0f5ea903865e70d2bc
Eoin Kelly <eoin@rabidtech.co.nz>
2013-11-07 14:37:43 +1300

    Temporarily stub out TinyMCE dependency.

---
+++ app/helpers/tiny_mce_helper.rb
@@ -0,0 +1,6 @@
+module TinyMceHelper
+  def include_tiny_mce_if_needed(*args)
+    # RABID: stubbing this method to get the minimal Rails 3 version up & running
+    # RABID: TODO: add tinymce back into the project
+  end
+end
