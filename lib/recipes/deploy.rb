b41d9c2ce72cc8129425569cb35b51aba178bc3d
Walter McGinnis <wm@waltermcginnis.com>
2013-11-23 15:52:23 +1300

    Clear out mongrel cruft

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -1,43 +1,17 @@
 # general capistrano recipes to restart application server
-
 namespace :deploy do
-
-  desc 'Start Application Server'
+  desc "Start Application Server"
   task :start do
-    set_app_server
-    case app_server
-    when :apache
-      deploy.apache.start
-    when :mongrel
-      deploy.mongrel.start
-    end
+    deploy.apache.start
   end
 
-  desc 'Stop Application Server'
+  desc "Stop Application Server"
   task :stop do
-    set_app_server
-    case app_server
-    when :apache
-      deploy.apache.stop
-    when :mongrel
-      deploy.mongrel.stop
-    end
+    deploy.apache.stop
   end
 
-  desc 'Restart Application Server'
+  desc "Restart Application Server"
   task :restart do
-    set_app_server
-    case app_server
-    when :apache
-      deploy.apache.restart_app
-    when :mongrel
-      deploy.mongrel.restart
-    end
+    deploy.apache.restart_app
   end
-
-  def set_app_server
-    begin; app_server; rescue; set(:app_server, :apache); end
-    raise "Unknown Application Server #{app_server}" unless [:apache, :mongrel].include?(app_server)
-  end
-
 end
