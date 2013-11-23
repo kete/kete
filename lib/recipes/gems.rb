e770403da196e12ec6b6f3ebbb554a4949f8e1c3
Walter McGinnis <wm@waltermcginnis.com>
2013-11-23 15:41:00 +1300

    Reformat file

--- lib/recipes/gems.rb
+++ lib/recipes/gems.rb
@@ -1,19 +1,15 @@
 # capistrano recipes to manage gems
 
 namespace :deploy do
-
   namespace :gems do
-
-    desc 'Install Required Gems'
+    desc "Install Required Gems"
     task :install, :role => :app do
       bundle install
     end
 
-    desc 'Update Required Gems'
+    desc "Update Required Gems"
     task :update, :role => :app do
       bundle update
     end
-
   end
-
 end
