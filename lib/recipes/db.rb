fc8cd5ab324c1c6c7e5380afee350f41655436d4
Walter McGinnis <wm@waltermcginnis.com>
2013-11-23 15:39:46 +1300

    Change rake to be bundler aware

--- lib/recipes/db.rb
+++ lib/recipes/db.rb
@@ -1,19 +1,15 @@
 # capistrano recipes specific to database server
 
 namespace :deploy do
-
   namespace :db do
-
     desc 'Bootstrap the Database'
     task :bootstrap, :roles => :app do
       set_app_environment
-      run "cd #{current_path} && RAILS_ENV=#{app_environment} rake db:bootstrap"
+      run "cd #{current_path} && RAILS_ENV=#{app_environment} #{rake} db:bootstrap"
     end
 
     def set_app_environment
       begin; app_environment; rescue; set(:app_environment, 'production'); end
     end
-
   end
-
 end
