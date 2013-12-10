f77a5e6424c60274c64639d46d76eda0b44b21cc
Eoin Kelly <eoin@rabidtech.co.nz>
2013-12-10 15:22:43 +1300

    Stop using SystemSetting.site_name in generated URLs

--- lib/default_url_options.rb
+++ lib/default_url_options.rb
@@ -1,8 +1,10 @@
 module DefaultUrlOptions
   unless included_modules.include? DefaultUrlOptions
+
+    # EOIN: ROB: we are pretty sure we don't need this
     # Sets the host for all url_for calls
-    def default_url_options(options = nil)
-      (defined?(SystemSetting.site_name) && SystemSetting.site_name.present?) ? { :host => SystemSetting.site_name } : {}
-    end
+    # def default_url_options(options = nil)
+    #   (defined?(SystemSetting.site_name) && SystemSetting.site_name.present?) ? { :host => SystemSetting.site_name } : {}
+    # end
   end
 end
