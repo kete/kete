cc87389454e69fafcd90c9c47e1e9dd607946e37
Eoin Kelly <eoin@rabidtech.co.nz>
2014-07-30 16:38:53 +1200

    Add failing spec

---
+++ spec/horowhenau/features/page_footer_links_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "Page footer Links" do
+  it "displays the links" do
+    visit "/"
+    expect(page).to have_text 'Accessibility'
+    expect(page).to have_text 'Sitemap'
+  end
+
+  it "Accessibility link works" do
+    visit "/"
+    click_on 'Accessibility'
+    expect(page).to have_text 'Accessibility features of this website'
+  end
+end
