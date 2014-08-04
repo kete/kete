b543e72d1c9a0c2e92541563da3ab800974752df
Eoin Kelly <eoin@rabidtech.co.nz>
2014-08-04 16:54:43 +1200

    DRY up spec for history pages

--- spec/horowhenau/features/history_spec.rb
+++ spec/horowhenau/features/history_spec.rb
@@ -1,50 +1,31 @@
 require 'spec_helper'
 
 feature "History" do
-  it "Topic history works" do
-    visit "/en/topics/196-levin-pottery-club" # a topic
-    click_on('History')
 
-    # the page loads correctly
-    expect(page.status_code).to be 200
-    expect(page).to have_text "Revision History:"
+  examples = {
+    audio: '/en/audio/2-anzac-speech-by-dominique-cooreman',
+    video: '/en/videos/57-daft-young-folk-band-perform',
+    topic: '/en/topics/196-levin-pottery-club',
+    image: '/en/images/20237-the-levin-orchid-society-show-august-2012-0241',
+    document: '/en/documents/194-page-8-50th-jubilee-commemoration-supplement',
+    web_link: '/en/web_links/23-organic-river-festival'
+  }
 
-    # there is a revision author avatar
-    expect(page).to have_selector('.history-table .contributor .user_contribution_link_avatar')
+  examples.each do |type, url|
+    it "#{type} history works correctly" do
+      visit url
+      click_on('History')
 
-    # There is a revision author name
-    expect(find('.history-table').find('.contributor', match: :first).text.length).to be > 0
+      # the page loads correctly
+      expect(page.status_code).to be 200
+      expect(page).to have_text "Revision History:"
+
+      # there is a revision author avatar
+      expect(page).to have_selector('.history-table .contributor .user_contribution_link_avatar')
+
+      # There is a revision author name
+      expect(find('.history-table').find('.contributor', match: :first).text.length).to be > 0
+    end
   end
 
-  it "Image history works" do
-    visit "/en/images/20237-the-levin-orchid-society-show-august-2012-0241" # an image
-
-    click_on('History')
-
-    # the page loads correctly
-    expect(page.status_code).to be 200
-    expect(page).to have_text "Revision History:"
-
-    # there is a revision author avatar
-    expect(page).to have_selector('.history-table .contributor .user_contribution_link_avatar')
-
-    # There is a revision author name
-    expect(find('.history-table').find('.contributor', match: :first).text.length).to be > 0
-  end
-
-  it "Document history works" do
-    visit "/en/documents/194-page-8-50th-jubilee-commemoration-supplement" # a document
-
-    click_on('History')
-
-    # the page loads correctly
-    expect(page.status_code).to be 200
-    expect(page).to have_text "Revision History:"
-
-    # there is a revision author avatar
-    expect(page).to have_selector('.history-table .contributor .user_contribution_link_avatar')
-
-    # There is a revision author name
-    expect(find('.history-table').find('.contributor', match: :first).text.length).to be > 0
-  end
 end
