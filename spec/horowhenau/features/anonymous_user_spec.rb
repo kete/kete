a8977b486776f57fa90b7b33a52a4f42fe7df38a
Eoin Kelly <eoin@rabidtech.co.nz>
2014-09-15 13:18:29 +1200

    Stop allowing anonymous users to see member list (fixes #78787566)

---
+++ spec/horowhenau/features/anonymous_user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Anonymous users' do
+
+  it 'are asked to login if they try to see list of members' do
+    visit '/en/site/members/list'
+    expect(current_path).to eq('/en/site/baskets/permission_denied')
+  end
+
+
+  it 'cannot see a link to the list of members on the homepage' do
+    visit '/'
+    expect(page).to_not have_link('Members')
+  end
+end
