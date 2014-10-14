5caa8c6be30c2621a75e38f47c8dc843b859843a
Rob Ramsay <robert.ramsay.nz@gmail.com>
2014-10-15 10:32:17 +1300

    Fix redundant test in secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-if Rails.env.test? || Rails.env.development? || Rails.env.development?
+if Rails.env.test? || Rails.env.development?
   KeteApp::Application.config.secret_token = 'a05fb67d1237cf87cf04a30a7a141d3c1377ae6db1985f15fefa745684790c320e672bda6d9201eea2013f3936bdf57f834006ab4df473c4590cb79944e12a52'
 else
   raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
