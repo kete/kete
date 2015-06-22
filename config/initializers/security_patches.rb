cff15b9f56d91cd1325188c8d907164147a97e81
Rob Ramsay <robert.ramsay.nz@gmail.com>
2015-06-22 16:15:44 +1200

    add security patch for CVE-2015-3226
    manual workaround for:
    
    [CVE-2015-3226] XSS Vulnerability in ActiveSupport::JSON.encode

---
+++ config/initializers/security_patches.rb
@@ -0,0 +1,13 @@
+# Workaround for: [CVE-2015-3226] XSS Vulnerability in ActiveSupport::JSON.encode
+module ActiveSupport
+  module JSON
+    module Encoding
+      private
+      class EscapedString
+        def to_s
+          self
+        end
+      end
+    end
+  end
+end
